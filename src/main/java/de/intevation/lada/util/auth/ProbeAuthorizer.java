9556829fe7236f7f6019321401042477500f9c34
Tom Gottfried <Tom Gottfried tom@intevation.de>
2021-01-12 17:41:09 +0100

    Revert "Use delete marker for authorization of a probe, returned by given id"
    This reverts commit d1f8e1326a00af3abbb74098d7761d26ed4eea21.

--- src/main/java/de/intevation/lada/util/auth/ProbeAuthorizer.java
+++ src/main/java/de/intevation/lada/util/auth/ProbeAuthorizer.java
@@ -78,8 +78,7 @@ public class ProbeAuthorizer extends BaseAuthorizer {
      */
     private Probe setAuthData(UserInfo userInfo, Probe probe) {
         MessStelle mst = repository.getByIdPlain(MessStelle.class, probe.getMstId(), Strings.STAMM);
-        if (!userInfo.getNetzbetreiber().contains(mst.getNetzbetreiberId())
-            || probe.isDeleted()) {
+        if (!userInfo.getNetzbetreiber().contains(mst.getNetzbetreiberId())) {
             probe.setOwner(false);
             probe.setReadonly(true);
             return probe;
