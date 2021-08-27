d6f5619e38703c7c884959a6997828d5d22432e1
Michael Stanko <mstanko@bfs.de>
2021-08-27 06:27:49 +0200

    partially revert of commit fb2845f5
    remove NOT NULL constraints from boolean columns of stamm.grid_column_values

--- db_schema/updates/0020/04_alter_grid_column_valuesd.sql
+++ db_schema/updates/0020/04_alter_grid_column_valuesd.sql
@@ -20,12 +20,7 @@ WHERE filter_is_null IS NULL;
 
 ALTER TABLE stamm.grid_column_values
     ALTER COLUMN filter_active SET DEFAULT false,
-    ALTER COLUMN filter_active SET NOT NULL,
     ALTER COLUMN visible SET DEFAULT false,
-    ALTER COLUMN visible SET NOT NULL,
     ALTER COLUMN filter_negate SET DEFAULT false,
-    ALTER COLUMN filter_negate SET NOT NULL,
     ALTER COLUMN filter_regex SET DEFAULT false,
-    ALTER COLUMN filter_regex SET NOT NULL,
-    ALTER COLUMN filter_is_null SET DEFAULT false,
-    ALTER COLUMN filter_is_null SET NOT NULL;
+    ALTER COLUMN filter_is_null SET DEFAULT false;
