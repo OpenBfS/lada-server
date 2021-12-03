673aa2252cac5f5ad05f513d1b42b86ad0d72b4a
Alexander Woestmann <awoestmann@intevation.de>
2021-12-03 14:23:58 +0100

    Remove unused import in LafImportJob

--- src/main/java/de/intevation/lada/importer/laf/LafImportJob.java
+++ src/main/java/de/intevation/lada/importer/laf/LafImportJob.java
@@ -23,7 +23,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
