e1684432da0a3944fcc48012cc01d7ab47da17cf
Arne Schumacher <aschumacher@bfs.de>
2022-01-28 10:23:33 +0100

    Add richtwert and sollist tables to stamm schema

--- db_schema/updates/0026/01.alter_stamm_tables.sql
+++ db_schema/updates/0026/01.alter_stamm_tables.sql
@@ -16,66 +16,81 @@ ALTER TABLE stamm.mess_stelle
 ALTER TABLE stamm.ortszusatz
     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.koordinaten_art
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.mess_einheit
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.mass_einheit_umrechnung
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.betriebsart
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.staat
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.netz_betreiber
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.umwelt
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.auth_funktion
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.auth_lst_umw
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.datenbasis
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.deskriptor_umwelt
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.mess_methode
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.messgroesse
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.messgroessen_gruppe
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.mg_grp
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.mmt_messgroesse_grp
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.ortszuordnung_typ
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.pflicht_messgroesse
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.rei_progpunkt
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.rei_progpunkt_gruppe
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.rei_progpunkt_grp_zuord
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.rei_progpunkt_grp_umw_zuord
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.kta
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.kta_gruppe
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.kta_grp_zuord
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.ort_typ
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.proben_zusatz
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.umwelt_zusatz
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.probenart
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.zeitbasis
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.importer_config
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE stamm.tm_fm_umrechnung
-	 ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+ALTER TABLE stamm.richtwert_massnahme
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+ALTER TABLE stamm.richtwert
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+ALTER TABLE stamm.sollist_mmtgrp
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+ALTER TABLE stamm.sollist_mmtgrp_zuord
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+ALTER TABLE stamm.sollist_umwgrp
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+ALTER TABLE stamm.sollist_umwgrp_zuord
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+ALTER TABLE stamm.sollist_soll
+     ADD COLUMN IF NOT EXISTS letzte_aenderung timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc');
+     
