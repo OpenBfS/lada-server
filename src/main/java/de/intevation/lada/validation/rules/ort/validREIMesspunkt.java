OpenBfS/lada-server

commit b6c155459b9d10eddb0264895ab8c1d5ec1bae29

Added validation rule for REIMesspunkt
	# Bitte geben Sie eine Versionsbeschreibung für Ihre Änderungen ein. Zeilen,
	# die mit '#' beginnen, werden ignoriert, und eine leere Versionsbeschreibung
	# bricht die Eintragung ab.
	# Auf Zweig june_2022
	# zum Eintragen bereitgestellte Änderungen:
	#   (benutzen Sie "git reset HEAD^1 <Datei>..." zum Herausnehmen aus der Bereitstellung)
	#
	#	neue Datei:   src/main/java/de/intevation/lada/validation/rules/ort/validREIMesspunkt.java
	#
	# Änderungen, die nicht zum Eintragen bereitgestellt sind:

src/main/java/de/intevation/lada/validation/rules/ort/validREIMesspunkt.java
@@ -1,0 +1,66 @@
+/* Copyright (C) 2013 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY! Check out
+ * the documentation coming with IMIS-Labordaten-Application for details.
+ */
+package de.intevation.lada.validation.rules.ort;
+
+import javax.inject.Inject;
+
+import de.intevation.lada.model.stammdaten.Ort;
+import de.intevation.lada.model.stammdaten.Kta;
+//import de.intevation.lada.util.data.QueryBuilder;
+import de.intevation.lada.util.data.Repository;
+import de.intevation.lada.util.data.StatusCodes;
+import de.intevation.lada.validation.Violation;
+import de.intevation.lada.validation.annotation.ValidationRule;
+import de.intevation.lada.validation.rules.Rule;
+
+@ValidationRule("Ort")
+public class validREIMesspunkt implements Rule {
+
+    @Inject
+    private Repository repository;
+
+    @Override
+    public Violation execute(Object object) {
+        Ort ort = (Ort) object;
+        String kta = null;
+
+        //check ortType
+        if (ort.getOrtTyp()!=3){
+            return null;
+        }
+
+        Violation violation = new Violation();
+        //getkta
+        if (ort.getKtaGruppeId()!= null){
+            kta = repository.getByIdPlain(Kta.class, ort.getKtaGruppeId()).getCode();
+        } else {
+            violation.addWarning("kta#kta_missing", StatusCodes.VALUE_MISSING);
+        }
+
+        //check ortId
+        if ( ort.getOrtId() == null) {
+            violation.addWarning("ortId", StatusCodes.VALUE_MISSING);
+        } else {
+            String mpId = ort.getOrtId();
+            if (mpId.length()>12){
+                violation.addWarning("ortId#tooLong", StatusCodes.VALUE_OUTSIDE_RANGE);
+            }
+            if (kta!=null && !mpId.substring(0, 3).equals(kta) ) {
+                violation.addWarning("kta#kta_notMatching", StatusCodes.VALUE_NOT_MATCHING);
+            }
+            if (mpId.substring(0, 4).equals(kta) && mpId.length() == 4) {
+                violation.addWarning("ortId#MP_missing", StatusCodes.VALUE_OUTSIDE_RANGE);
+            }
+            if (mpId.substring(0, 4).equals(kta) && mpId.substring(4, mpId.length()).length() > 4) {
+                violation.addNotification("ortId#MP_tooLong", StatusCodes.VALUE_OUTSIDE_RANGE);
+            }
+        }
+
+    return violation;
+    }
+}

commit ac0bb7909aa37ab730f8f76a83214e01e89d211e

clean up and prevention of a server exception in validation rule for REIMesspunkt (introduced with b6c1554)

src/main/java/de/intevation/lada/validation/rules/ort/validREIMesspunkt.java
@@ -39,26 +39,35 @@ public class validREIMesspunkt implements Rule {
         if (ort.getKtaGruppeId()!= null){
             kta = repository.getByIdPlain(Kta.class, ort.getKtaGruppeId()).getCode();
         } else {
-            violation.addWarning("kta#kta_missing", StatusCodes.VALUE_MISSING);
+            violation.addWarning("ktaGruppeId", StatusCodes.VALUE_MISSING);
         }
 
         //check ortId
-        if ( ort.getOrtId() == null) {
-            violation.addWarning("ortId", StatusCodes.VALUE_MISSING);
-        } else {
+        if (ort.getOrtId() == null) {
+            if (ort.getId() != null) {
+                //should not happen due to db constraint!
+                //if getId() == null we have a new Ort which wil be validated before creation
+                violation.addWarning("ortId", StatusCodes.VALUE_MISSING);
+                return violation;
+            }
+        } else if (ort.getOrtId().length() < 5){
+            violation.addWarning("ortId", StatusCodes.VALUE_OUTSIDE_RANGE);
+        } else if (ort.getOrtId().length()> 5){
             String mpId = ort.getOrtId();
             if (mpId.length()>12){
-                violation.addWarning("ortId#tooLong", StatusCodes.VALUE_OUTSIDE_RANGE);
+                violation.addWarning("ortId", StatusCodes.VALUE_OUTSIDE_RANGE);
             }
             if (kta!=null && !mpId.substring(0, 3).equals(kta) ) {
-                violation.addWarning("kta#kta_notMatching", StatusCodes.VALUE_NOT_MATCHING);
+                violation.addWarning("ortId", StatusCodes.VALUE_NOT_MATCHING);
             }
             if (mpId.substring(0, 4).equals(kta) && mpId.length() == 4) {
-                violation.addWarning("ortId#MP_missing", StatusCodes.VALUE_OUTSIDE_RANGE);
+                violation.addWarning("ortId", StatusCodes.VALUE_OUTSIDE_RANGE);
             }
             if (mpId.substring(0, 4).equals(kta) && mpId.substring(4, mpId.length()).length() > 4) {
-                violation.addNotification("ortId#MP_tooLong", StatusCodes.VALUE_OUTSIDE_RANGE);
+                violation.addNotification("ortId", StatusCodes.VALUE_OUTSIDE_RANGE);
             }
+        } else {
+            violation.addWarning("ortId", StatusCodes.VALUE_OUTSIDE_RANGE);
         }
 
     return violation;