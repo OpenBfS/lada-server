526d72f21634353a35576cd42e71dd2b37778997
Alexander Woestmann <awoestmann@intevation.de>
2022-11-17 13:41:00 +0100

    Update Poi model schema name

--- src/main/java/de/intevation/lada/model/stammdaten/Poi.java
+++ src/main/java/de/intevation/lada/model/stammdaten/Poi.java
@@ -16,7 +16,7 @@ import javax.persistence.Table;
 import java.sql.Timestamp;
 
 @Entity
-@Table(name = "poi", schema = SchemaName.LEGACY_NAME)
+@Table(name = "poi", schema = SchemaName.NAME)
 public class Poi implements Serializable {
     private static final long serialVersionUID = 1L;
 
