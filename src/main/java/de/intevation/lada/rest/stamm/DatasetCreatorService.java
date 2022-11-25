2d70a4d38ac74e45546645fe4c1f4fee9429d4db
Alexander Woestmann <awoestmann@intevation.de>
2022-11-25 14:12:48 +0100

    Rename DatensatzErzeugerService to DatasetCreatorService

--- src/main/java/de/intevation/lada/rest/stamm/DatasetCreatorService.java
+++ src/main/java/de/intevation/lada/rest/stamm/DatasetCreatorService.java
@@ -37,7 +37,7 @@ import de.intevation.lada.rest.LadaService;
  * @author <a href="mailto:rrenkert@intevation.de">Raimund Renkert</a>
  */
 @Path("rest/datasetcreator")
-public class DatensatzErzeugerService extends LadaService {
+public class DatasetCreatorService extends LadaService {
 
     /**
      * The data repository granting read access.
