f05d7d181c68ce0675cd5aad615973f132662b62
Alexander Woestmann <awoestmann@intevation.de>
2022-11-28 10:21:56 +0100

    Rename MessgroesseService to MeasdService

--- src/main/java/de/intevation/lada/rest/stamm/MeasdService.java
+++ src/main/java/de/intevation/lada/rest/stamm/MeasdService.java
@@ -29,7 +29,7 @@ import de.intevation.lada.rest.LadaService;
  * @author <a href="mailto:rrenkert@intevation.de">Raimund Renkert</a>
  */
 @Path("rest/measd")
-public class MessgroesseService extends LadaService {
+public class MeasdService extends LadaService {
 
     /**
      * The data repository granting read access.
