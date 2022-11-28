f5dfe7b344e25a5403026c46a5a0616ec59e44d8
Alexander Woestmann <awoestmann@intevation.de>
2022-11-28 10:03:47 +0100

    Rename MesseinheitService to MeasUnitService

--- src/main/java/de/intevation/lada/rest/stamm/MeasUnitService.java
+++ src/main/java/de/intevation/lada/rest/stamm/MeasUnitService.java
@@ -29,7 +29,7 @@ import de.intevation.lada.rest.LadaService;
  * @author <a href="mailto:rrenkert@intevation.de">Raimund Renkert</a>
  */
 @Path("rest/measunit")
-public class MesseinheitService extends LadaService {
+public class MeasUnitService extends LadaService {
 
     /**
      * The data repository granting read access.
