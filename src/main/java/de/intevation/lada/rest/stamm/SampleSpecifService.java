5a2be24f95d74728d69d9b3e4cdc5cb9e6aec71e
Alexander Woestmann <awoestmann@intevation.de>
2022-11-28 12:43:40 +0100

    Rename ProbenzusatzService to SampleSpecifService

--- src/main/java/de/intevation/lada/rest/stamm/SampleSpecifService.java
+++ src/main/java/de/intevation/lada/rest/stamm/SampleSpecifService.java
@@ -28,7 +28,7 @@ import de.intevation.lada.rest.LadaService;
  * @author <a href="mailto:rrenkert@intevation.de">Raimund Renkert</a>
  */
 @Path("rest/samplespecif")
-public class ProbenzusatzService extends LadaService {
+public class SampleSpecifService extends LadaService {
 
     /**
      * The data repository granting read access.
