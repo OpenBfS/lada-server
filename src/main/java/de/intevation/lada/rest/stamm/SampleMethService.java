03fe14a658f6ff91abf286a2cbf35a3383d511b1
Alexander Woestmann <awoestmann@intevation.de>
2022-11-28 12:31:31 +0100

    Rename ProbenartService to SampleMethService

--- src/main/java/de/intevation/lada/rest/stamm/SampleMethService.java
+++ src/main/java/de/intevation/lada/rest/stamm/SampleMethService.java
@@ -23,7 +23,7 @@ import de.intevation.lada.rest.LadaService;
  * @author <a href="mailto:rrenkert@intevation.de">Raimund Renkert</a>
  */
 @Path("rest/samplemeth")
-public class ProbenartService extends LadaService {
+public class SampleMethService extends LadaService {
 
     /**
      * The data repository granting read access.
