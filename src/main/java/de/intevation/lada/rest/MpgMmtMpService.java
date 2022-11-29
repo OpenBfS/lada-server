5f8a3862b03e8a41339f8a58f41ad6fdc05e83a7
Alexander Woestmann <awoestmann@intevation.de>
2022-11-29 13:32:57 +0100

    Rename MessprogrammMmtService to MpgMmtMpService

--- src/main/java/de/intevation/lada/rest/MpgMmtMpService.java
+++ src/main/java/de/intevation/lada/rest/MpgMmtMpService.java
@@ -41,7 +41,7 @@ import de.intevation.lada.validation.annotation.ValidationConfig;
  * @author <a href="mailto:rrenkert@intevation.de">Raimund Renkert</a>
  */
 @Path("rest/mpgmmtmp")
-public class MessprogrammMmtService extends LadaService {
+public class MpgMmtMpService extends LadaService {
 
     /**
      * The data repository granting read/write access.
