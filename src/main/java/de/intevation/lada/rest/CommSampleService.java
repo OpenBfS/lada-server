bdc93b1e69bdefb6c9861e37414dee6eb740cb64
Alexander Woestmann <awoestmann@intevation.de>
2022-11-29 13:28:06 +0100

    Rename KommentarPService to CommSampleService

--- src/main/java/de/intevation/lada/rest/CommSampleService.java
+++ src/main/java/de/intevation/lada/rest/CommSampleService.java
@@ -36,7 +36,7 @@ import de.intevation.lada.validation.annotation.ValidationConfig;
  * @author <a href="mailto:rrenkert@intevation.de">Raimund Renkert</a>
  */
 @Path("rest/commsample")
-public class KommentarPService extends LadaService {
+public class CommSampleService extends LadaService {
 
     /**
      * The data repository granting read/write access.
