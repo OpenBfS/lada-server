5ec4e28016e7625daada84ca1faf8caf86860563
Alexander Woestmann <awoestmann@intevation.de>
2022-11-29 14:23:30 +0100

    Rename StatusService to StatusProtService

--- src/main/java/de/intevation/lada/rest/StatusProtService.java
+++ src/main/java/de/intevation/lada/rest/StatusProtService.java
@@ -49,7 +49,7 @@ import de.intevation.lada.validation.annotation.ValidationConfig;
  * @author <a href="mailto:rrenkert@intevation.de">Raimund Renkert</a>
  */
 @Path("rest/statusprot")
-public class StatusService extends LadaService {
+public class StatusProtService extends LadaService {
 
     /**
      * The data repository granting read/write access.
