83ee551bcd196de044daf85b91d80ad38ac28376
Alexander Woestmann <awoestmann@intevation.de>
2022-11-29 13:42:20 +0100

    Rename MessungService to MeasmService

--- src/main/java/de/intevation/lada/rest/MeasmService.java
+++ src/main/java/de/intevation/lada/rest/MeasmService.java
@@ -41,7 +41,7 @@ import de.intevation.lada.validation.annotation.ValidationConfig;
  * @author <a href="mailto:rrenkert@intevation.de">Raimund Renkert</a>
  */
 @Path("rest/measm")
-public class MessungService extends LadaService {
+public class MeasmService extends LadaService {
 
     /**
      * The data repository granting read/write access.
