OpenBfS/lada-server

commit f9f66a092bd058bb751b29af6406c85f73773d95

Count references to Messprogramm objects directly in the model
Makes this property really related to data. Before, if a client issued a
POST request with a wrong value (e.g. other than zero, because a new object
cannot be referenced already), that value was kept in the response
if the response still referenced the very same instance created from the data
in the POST request.
In passing, fix some code style issues.

src/main/java/de/intevation/lada/model/land/Messprogramm.java
@@ -9,12 +9,16 @@ package de.intevation.lada.model.land;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Transient;
@@ -120,8 +124,13 @@ public class Messprogramm implements Serializable {
     @Column(name = "probenahmemenge")
     private String probenahmeMenge;
 
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "mpr_id")
+    private Set<Probe> proben;
+
     @Transient
-    private Integer referenceCount;
+    @JsonIgnore
+    private int referenceCount;
 
     @Transient
     @JsonIgnore
@@ -345,12 +354,9 @@ public class Messprogramm implements Serializable {
         this.probenahmeMenge = probenahmeMenge;
     }
 
-    public Integer getReferenceCount() {
-        return this.referenceCount;
-    }
-
-    public void setReferenceCount(Integer referenceCount) {
-        this.referenceCount = referenceCount;
+    @JsonProperty
+    public int getReferenceCount() {
+        return this.proben.size();
     }
 
     @JsonProperty

src/main/java/de/intevation/lada/rest/MessprogrammService.java
@@ -186,13 +186,6 @@ public class MessprogrammService extends LadaService {
                 repository.getById(
                     Messprogramm.class, Integer.valueOf(id)),
                 Messprogramm.class);
-        Messprogramm mp = (Messprogramm) response.getData();
-        QueryBuilder<Probe> builder = repository.queryBuilder(Probe.class);
-        builder.and("mprId", mp.getId());
-        List<Probe> probes =
-            repository.filterPlain(builder.getQuery());
-        mp.setReferenceCount(probes.size());
-        response.setData(mp);
         return response;
     }
 
@@ -258,12 +251,13 @@ public class MessprogrammService extends LadaService {
         ) {
             messprogramm = factory.findUmweltId(messprogramm);
         } else if ((messprogramm.getUmwId() != null
-        || !messprogramm.getUmwId().equals(""))
-        && (messprogramm.getMediaDesk() == null
-        || messprogramm.getMediaDesk().equals(""))
-        ){
+                || !messprogramm.getUmwId().equals(""))
+            && (messprogramm.getMediaDesk() == null
+                || messprogramm.getMediaDesk().equals(""))
+        ) {
             messprogramm = factory.getInitialMediaDesk(messprogramm);
         }
+
         /* Persist the new messprogramm object*/
         Response response = repository.create(messprogramm);
         Messprogramm ret = (Messprogramm) response.getData();
@@ -334,16 +328,16 @@ public class MessprogrammService extends LadaService {
         }
 
         if ((messprogramm.getUmwId() == null
-            || messprogramm.getUmwId().equals(""))
+                || messprogramm.getUmwId().equals(""))
             && !(messprogramm.getMediaDesk() == null
-            || messprogramm.getMediaDesk().equals(""))
+                || messprogramm.getMediaDesk().equals(""))
         ) {
             messprogramm = factory.findUmweltId(messprogramm);
         } else if (!(messprogramm.getUmwId() == null
-            || messprogramm.getUmwId().equals(""))
-            && ( messprogramm.getMediaDesk() == null
-            || messprogramm.getMediaDesk().equals(""))
-            ){
+                || messprogramm.getUmwId().equals(""))
+            && (messprogramm.getMediaDesk() == null
+                || messprogramm.getMediaDesk().equals(""))
+            ) {
             messprogramm = factory.getInitialMediaDesk(messprogramm);
         }
         Response response = repository.update(messprogramm);

commit d6131e20c788deec8c7b08f8e9d34e2c7cc3319d

fix for commit f9f66a092, which accidentally introduced the removal of references for mpr-id in probeobjects on messprogramm updates

src/main/java/de/intevation/lada/model/land/Messprogramm.java
@@ -127,7 +127,7 @@ public class Messprogramm implements Serializable {
     private String probenahmeMenge;
 
     @OneToMany(fetch = FetchType.EAGER)
-    @JoinColumn(name = "mpr_id")
+    @JoinColumn(name = "mpr_id", updatable = false, insertable = false)
     private Set<Probe> proben;
 
     @ManyToMany (fetch = FetchType.EAGER)