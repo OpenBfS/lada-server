7b28c4593e0c548ade46f1f377ceab68e2c3868f
Tom Gottfried <Tom Gottfried tom@intevation.de>
2023-01-23 16:50:12 +0100

    Let PostgreSQL choose constraint names
    The schema script does not give these names, neither.

--- db_schema/updates/0045/01_add_unique_constraints_to_mapping_tables.sql
+++ db_schema/updates/0045/01_add_unique_constraints_to_mapping_tables.sql
@@ -1,21 +1,17 @@
 ALTER TABLE master.query_meas_facil_mp
-    ADD CONSTRAINT query_meas_facil_mp_query_id_meas_facil_id_unique
-    UNIQUE (query_id, meas_facil_id);
+    ADD UNIQUE (query_id, meas_facil_id);
 
 ALTER TABLE master.rei_ag_gr_mp
-    ADD CONSTRAINT rei_ag_gr_mp_rei_ag_gr_id_rei_ag_id_unique
-        UNIQUE(rei_ag_gr_id, rei_ag_id),
+    ADD UNIQUE(rei_ag_gr_id, rei_ag_id),
     ALTER COLUMN rei_ag_gr_id SET NOT NULL,
     ALTER COLUMN rei_ag_id SET NOT NULL;
 
 ALTER TABLE master.rei_ag_gr_env_medium_mp
-    ADD CONSTRAINT rei_ag_gr_env_medium_mp_rei_ag_gr_id_env_medium_id_unique
-        UNIQUE(rei_ag_gr_id, env_medium_id),
+    ADD UNIQUE(rei_ag_gr_id, env_medium_id),
     ALTER COLUMN rei_ag_gr_id SET NOT NULL,
     ALTER COLUMN env_medium_id SET NOT NULL;
 
 ALTER TABLE master.nucl_facil_gr_mp
-    ADD CONSTRAINT nucl_facil_gr_mp_nucl_facil_gr_id_nucl_facil_id_unique
-        UNIQUE(nucl_facil_gr_id, nucl_facil_id),
+    ADD UNIQUE(nucl_facil_gr_id, nucl_facil_id),
     ALTER COLUMN nucl_facil_gr_id SET NOT NULL,
     ALTER COLUMN nucl_facil_id SET NOT NULL;
