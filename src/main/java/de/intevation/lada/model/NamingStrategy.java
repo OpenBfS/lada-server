23a0bb02c50efcc97933bbb9202ce01d04e126fc
Alexander Woestmann <awoestmann@intevation.de>
2023-01-26 11:57:48 +0100

    Update NamingStrategy toIdentifier method to properly handle snake case input
    The updated method should remove the need of further overrides in the NamingStrategy class
    as it can now handle snake and camel case input

--- src/main/java/de/intevation/lada/model/NamingStrategy.java
+++ src/main/java/de/intevation/lada/model/NamingStrategy.java
@@ -10,7 +10,6 @@ package de.intevation.lada.model;
 import org.apache.commons.lang3.StringUtils;
 
 import org.hibernate.boot.model.naming.Identifier;
-import org.hibernate.boot.model.naming.ImplicitJoinColumnNameSource;
 import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
 import org.hibernate.boot.spi.MetadataBuildingContext;
 
@@ -23,36 +22,16 @@ public class NamingStrategy extends ImplicitNamingStrategyJpaCompliantImpl {
     protected Identifier toIdentifier(
         String stringForm, MetadataBuildingContext buildingContext
     ) {
-        String[] words = StringUtils.splitByCharacterTypeCamelCase(stringForm);
-        // Names are not case sensitive, so leave the words capitalized
-        String snake = String.join("_", words);
+        //Split input by "_" in case input is already in snake case
+        String[] parts = stringForm.split("_");
+        //Convert each part into snake case
+        for (int i = 0; i < parts.length; i++) {
+            String part = parts[i];
+            String[] words = StringUtils.splitByCharacterTypeCamelCase(part);
+            // Names are not case sensitive, so leave the words capitalized
+            parts[i] = String.join("_", words);
+        }
+        String snake = String.join("_", parts);
         return super.toIdentifier(snake, buildingContext);
-    }
-
-    /**
-     * Determine join column name from the give source.
-     *
-     * The join column name is generated using the following format:
-     *
-     * {name_of_the_referenced_entity}_{referenced_entity_primary_key}
-     *
-     * and is returned in snake_case
-     */
-    @Override
-    public Identifier determineJoinColumnName(ImplicitJoinColumnNameSource source) {
-        final String name;
-
-        //Generate camel case representation of the join column
-        if ( source.getNature() == ImplicitJoinColumnNameSource.Nature.ELEMENT_COLLECTION
-                || source.getAttributePath() == null ) {
-            name = transformEntityName( source.getEntityNaming() )
-                    + StringUtils.capitalize(source.getReferencedColumnName().getText());
-        }
-        else {
-            name = transformAttributePath( source.getAttributePath() )
-                    + StringUtils.capitalize(source.getReferencedColumnName().getText());
-        }
-        //Transform to snake case identifier
-        return toIdentifier( name, source.getBuildingContext() );
     }
 }
