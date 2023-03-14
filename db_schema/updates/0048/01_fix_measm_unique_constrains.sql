00ee1dcf1a770f7e19ddf8558a98ca8a38a3aa53
Michael Stanko <mstanko@bfs.de>
2023-03-14 09:47:44 +0100

    fix commit c6212f669 - unique constraints of sample.measm

---
+++ db_schema/updates/0048/01_fix_measm_unique_constrains.sql
@@ -0,0 +1,11 @@
+SET role lada;
+
+ALTER TABLE lada.measm DROP CONSTRAINT measm_id_ext_id_key;
+
+ALTER TABLE lada.measm DROP CONSTRAINT measm_id_min_sample_id_key;
+
+ALTER TABLElada.measm
+    ADD CONSTRAINT measm_sample_id_ext_id_key UNIQUE (sample_id, ext_id);
+
+ALTER TABLE lada.measm
+    ADD CONSTRAINT measm_sample_id_min_sample_id_key UNIQUE (sample_id, min_sample_id);
