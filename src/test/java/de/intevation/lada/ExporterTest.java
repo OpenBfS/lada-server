550f03dc940f5fdf276bfb219d96d3f1a6bdad68
Tom Gottfried <Tom Gottfried tom@intevation.de>
2023-04-25 17:50:42 +0200

    Test asynchronous export of a Measm object with measVals

--- src/test/java/de/intevation/lada/ExporterTest.java
+++ src/test/java/de/intevation/lada/ExporterTest.java
@@ -89,6 +89,24 @@ public class ExporterTest extends BaseTest {
                 .add("isFilterRegex", false)
                 .add("gridColMpId", 4)));
 
+    private final JsonObject measmRequestJson = Json.createObjectBuilder()
+        .add("timezone", "UTC")
+        .add("columns", Json.createArrayBuilder()
+            .add(Json.createObjectBuilder()
+                .add("columnIndex", 0)
+                .add("export", true)
+                .add("filterVal", "")
+                .add("isFilterActive", false)
+                .add("isFilterNull", false)
+                .add("isFilterNegate", false)
+                .add("isFilterRegex", false)
+                .add("gridColMpId", 5)))
+        .add("idField", "messungId")
+        .add("idFilter", Json.createArrayBuilder().add("1200"))
+        .add("exportSubData", true)
+        .add("subDataColumns", Json.createArrayBuilder().add("id"))
+        .build();
+
     /**
      * Prepare data for export of a Sample object.
      */
@@ -205,6 +223,32 @@ public class ExporterTest extends BaseTest {
     }
 
     /**
+     * Test asynchronous CSV export of Measm objects including measVals.
+     */
+    @Test
+    @InSequence(3)
+    @RunAsClient
+    public final void testCsvExportMeasmSubData(
+        @ArquillianResource URL baseUrl
+    ) throws InterruptedException, CharacterCodingException {
+        Protocol prot = new Protocol();
+        prot.setName("asyncexport service");
+        prot.setType(formatCsv);
+        prot.setPassed(false);
+        testProtocol.add(prot);
+
+        String result = runExportTest(
+            baseUrl, formatCsv, prot, measmRequestJson);
+        Assert.assertEquals(
+            "Unexpected CSV content",
+            "messungId,id\r\n"
+            + "1200,1000\r\n",
+            result);
+
+        prot.setPassed(true);
+    }
+
+    /**
      * Test asynchronous JSON export of a Sample identified by ID.
      */
     @Test
@@ -268,6 +312,33 @@ public class ExporterTest extends BaseTest {
             + "\"umw_id\":\"L6\","
             + "\"probeId\":1000,"
             + "\"Messungen\":[{\"extId\":453}]}}",
+            result);
+
+        prot.setPassed(true);
+    }
+
+    /**
+     * Test asynchronous JSON export of a Measm object with measVals.
+     */
+    @Test
+    @InSequence(4)
+    @RunAsClient
+    public final void testJsonExportMeasmSubData(
+        @ArquillianResource URL baseUrl
+    ) throws InterruptedException, CharacterCodingException {
+        Protocol prot = new Protocol();
+        prot.setName("asyncexport service");
+        prot.setType(formatJson);
+        prot.setPassed(false);
+        testProtocol.add(prot);
+
+        String result = runExportTest(
+            baseUrl, formatJson, prot, measmRequestJson);
+        Assert.assertEquals(
+            "Unexpected JSON content",
+            "{\"1200\":"
+            + "{\"messungId\":1200,"
+            + "\"messwerte\":[{\"id\":1000}]}}",
             result);
 
         prot.setPassed(true);
