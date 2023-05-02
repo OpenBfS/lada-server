c7b5ebc4e19dafe97998043d03a8417cf2b38b7a
Tom Gottfried <Tom Gottfried tom@intevation.de>
2023-05-02 15:01:02 +0200

    Complement rev. 84acce95

--- src/main/java/de/intevation/lada/validation/MessungValidator.java
+++ src/main/java/de/intevation/lada/validation/MessungValidator.java
@@ -40,20 +40,6 @@ public class MessungValidator implements Validator {
             violations.addError("measm", StatusCodes.NOT_A_PROBE);
             return violations;
         }
-        for (Rule rule : rules) {
-            Violation result = rule.execute(object);
-            if (result != null) {
-                if (result.hasWarnings()) {
-                    violations.addWarnings(result.getWarnings());
-                }
-                if (result.hasErrors()) {
-                    violations.addErrors(result.getErrors());
-                }
-                if (result.hasNotifications()) {
-                  violations.addNotifications(result.getNotifications());
-                }
-            }
-        }
-        return violations;
+        return Validator.validate(object, rules);
     }
 }
