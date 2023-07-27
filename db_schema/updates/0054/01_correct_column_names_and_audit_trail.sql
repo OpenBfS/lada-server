f991decfd45859397a4b55d0e4a6f7f80f59d5d1
Tom Gottfried <Tom Gottfried tom@intevation.de>
2023-07-27 15:19:08 +0200

    Only rename constraints that do not already have the new name
    In case the database was set up with the new column names already,
    the constraints already have the new names as well, which leads to
    errors when trying to rename the constraints.

--- db_schema/updates/0054/01_correct_column_names_and_audit_trail.sql
+++ db_schema/updates/0054/01_correct_column_names_and_audit_trail.sql
@@ -5,8 +5,26 @@ SET row_data=public.jsonb_rename_keys(row_data, ARRAY['x_coord_ext', 'coord_x_ex
 changed_fields=public.jsonb_rename_keys(changed_fields, ARRAY['x_coord_ext', 'coord_x_ext', 'y_coord_ext', 'coord_y_ext'])
 WHERE table_name='site';
 
-ALTER TABLE lada.mpg RENAME CONSTRAINT mpg_state_mpg_id_fkey TO mpg_mpg_categ_id_fkey;
-ALTER TABLE lada.sample RENAME CONSTRAINT sample_state_mpg_id_fkey TO sample_mpg_categ_id_fkey;
+-- Rename constraints according to renaming columns in update #46
+DO $$
+DECLARE
+    old_name CONSTANT varchar = 'state_mpg';
+    new_name CONSTANT varchar = 'mpg_categ';
+    s varchar; t varchar; c varchar;
+BEGIN
+    FOR s, t, c IN SELECT tc.table_schema, tc.table_name, constraint_name
+        FROM information_schema.constraint_column_usage AS ccu
+            JOIN information_schema.table_constraints AS tc
+                USING (constraint_schema, constraint_name)
+        WHERE ccu.table_schema = 'master'
+            AND ccu.table_name = 'mpg_categ' AND ccu.column_name = 'id'
+    LOOP
+        IF c LIKE '%' || old_name || '%' THEN
+            EXECUTE format('ALTER TABLE %I.%I RENAME CONSTRAINT %I TO %I',
+                s, t, c, replace(c, old_name, new_name));
+        END IF;
+    END LOOP;
+END$$;
 
 ALTER TABLE IF EXISTS master.convers_dm_fm RENAME COLUMN unit_id TO meas_unit_id;
 ALTER TABLE IF EXISTS master.convers_dm_fm RENAME COLUMN to_unit_id TO to_meas_unit_id;
