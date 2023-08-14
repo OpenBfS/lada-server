ea8402c6e623a16ceabf504135da318e95318804
Tom Gottfried <Tom Gottfried tom@intevation.de>
2023-08-14 15:53:17 +0200

    Do not hide all possible exceptions in some tests

--- src/test/java/de/intevation/lada/ImporterTest.java
+++ src/test/java/de/intevation/lada/ImporterTest.java
@@ -323,32 +323,28 @@ public class ImporterTest extends BaseTest {
      */
     @Test
     public final void mergeProbe() throws Exception {
-        try {
-            transaction.begin();
-            Sample probe = new Sample();
-            probe.setExtId("T001");
-            probe.setMainSampleId("120510002");
-            probe.setMeasFacilId(mstId);
-            probe.setOprModeId(1);
-            probe.setRegulationId(DID9);
-            probe.setEnvDescripName(
-                "Trinkwasser Zentralversorgung Oberflächenwasser aufbereitet");
-            probe.setEnvDescripDisplay("D: 59 04 01 00 05 05 01 02 00 00 00 00");
-            probe.setMpgId(MPRID1000);
-            probe.setSamplerId(PNID);
-            probe.setIsTest(false);
-            probe.setApprLabId(mstId);
-            probe.setSampleMethId(2);
-            probe.setEnvMediumId("A6");
-            probe.setSchedStartDate(Timestamp.valueOf("2013-05-01 16:00:00"));
-            probe.setSchedEndDate(Timestamp.valueOf("2013-05-05 16:00:00"));
-            probe.setSampleStartDate(Timestamp.valueOf("2012-05-03 13:07:00"));
-            Sample dbProbe = repository.getByIdPlain(Sample.class, PID1000);
-            merger.merge(dbProbe, probe);
-            transaction.commit();
-        } catch (Exception e) {
-            transaction.rollback();
-        }
+        transaction.begin();
+        Sample probe = new Sample();
+        probe.setExtId("T001");
+        probe.setMainSampleId("120510002");
+        probe.setMeasFacilId(mstId);
+        probe.setOprModeId(1);
+        probe.setRegulationId(DID9);
+        probe.setEnvDescripName(
+            "Trinkwasser Zentralversorgung Oberflächenwasser aufbereitet");
+        probe.setEnvDescripDisplay("D: 59 04 01 00 05 05 01 02 00 00 00 00");
+        probe.setMpgId(MPRID1000);
+        probe.setSamplerId(PNID);
+        probe.setIsTest(false);
+        probe.setApprLabId(mstId);
+        probe.setSampleMethId(2);
+        probe.setEnvMediumId("A6");
+        probe.setSchedStartDate(Timestamp.valueOf("2013-05-01 16:00:00"));
+        probe.setSchedEndDate(Timestamp.valueOf("2013-05-05 16:00:00"));
+        probe.setSampleStartDate(Timestamp.valueOf("2012-05-03 13:07:00"));
+        Sample dbProbe = repository.getByIdPlain(Sample.class, PID1000);
+        merger.merge(dbProbe, probe);
+        transaction.commit();
 
         shouldMatchDataSet("datasets/dbUnit_import_merge_match.xml",
             "lada.sample", new String[]{"last_mod", "tree_mod", "mid_coll_pd"});
@@ -360,22 +356,18 @@ public class ImporterTest extends BaseTest {
      */
     @Test
     public final void mergeMessung() throws Exception {
-        try {
-            transaction.begin();
-            Measm messung = new Measm();
-            messung.setMinSampleId("06A0");
-            messung.setIsScheduled(true);
-            messung.setIsCompleted(false);
-            messung.setMeasPd(MDAUER1000);
-            messung.setMmtId("A3");
-            messung.setMeasmStartDate(Timestamp.valueOf("2012-05-06 14:00:00"));
-            Measm dbMessung =
-                repository.getByIdPlain(Measm.class, MID1200);
-            merger.mergeMessung(dbMessung, messung);
-            transaction.commit();
-        } catch (Exception e) {
-            transaction.rollback();
-        }
+        transaction.begin();
+        Measm messung = new Measm();
+        messung.setMinSampleId("06A0");
+        messung.setIsScheduled(true);
+        messung.setIsCompleted(false);
+        messung.setMeasPd(MDAUER1000);
+        messung.setMmtId("A3");
+        messung.setMeasmStartDate(Timestamp.valueOf("2012-05-06 14:00:00"));
+        Measm dbMessung =
+            repository.getByIdPlain(Measm.class, MID1200);
+        merger.mergeMessung(dbMessung, messung);
+        transaction.commit();
 
         shouldMatchDataSet(
             "datasets/dbUnit_import_merge_match_messung.xml",
@@ -392,38 +384,34 @@ public class ImporterTest extends BaseTest {
     @Test
     @Ignore
     public final void mergeZusatzwert() throws Exception {
-        try {
-            transaction.begin();
-            Sample probe = repository.getByIdPlain(Sample.class, PID1000);
-            List<SampleSpecifMeasVal> zusatzwerte = new ArrayList<SampleSpecifMeasVal>();
-            SampleSpecifMeasVal wert1 = new SampleSpecifMeasVal();
-            wert1.setSampleId(PID1000);
-            wert1.setError(MESSFEHLER12F);
-            wert1.setSmallerThan("<");
-            wert1.setSampleSpecifId("A74");
+        transaction.begin();
+        Sample probe = repository.getByIdPlain(Sample.class, PID1000);
+        List<SampleSpecifMeasVal> zusatzwerte = new ArrayList<SampleSpecifMeasVal>();
+        SampleSpecifMeasVal wert1 = new SampleSpecifMeasVal();
+        wert1.setSampleId(PID1000);
+        wert1.setError(MESSFEHLER12F);
+        wert1.setSmallerThan("<");
+        wert1.setSampleSpecifId("A74");
 
-            SampleSpecifMeasVal wert2 = new SampleSpecifMeasVal();
-            wert2.setSampleId(PID1000);
-            wert2.setError(MESSFEHLER02F);
-            wert2.setMeasVal(MESS18D);
-            wert1.setSmallerThan(null);
-            wert2.setSampleSpecifId("A75");
+        SampleSpecifMeasVal wert2 = new SampleSpecifMeasVal();
+        wert2.setSampleId(PID1000);
+        wert2.setError(MESSFEHLER02F);
+        wert2.setMeasVal(MESS18D);
+        wert1.setSmallerThan(null);
+        wert2.setSampleSpecifId("A75");
 
-            SampleSpecifMeasVal wert3 = new SampleSpecifMeasVal();
-            wert3.setSampleId(PID1000);
-            wert3.setError(MESSFEHLER02F);
-            wert3.setMeasVal(MESS18D);
-            wert1.setSmallerThan(null);
-            wert3.setSampleSpecifId("A76");
+        SampleSpecifMeasVal wert3 = new SampleSpecifMeasVal();
+        wert3.setSampleId(PID1000);
+        wert3.setError(MESSFEHLER02F);
+        wert3.setMeasVal(MESS18D);
+        wert1.setSmallerThan(null);
+        wert3.setSampleSpecifId("A76");
 
-            zusatzwerte.add(wert1);
-            zusatzwerte.add(wert2);
-            zusatzwerte.add(wert3);
-            merger.mergeZusatzwerte(probe, zusatzwerte);
-            transaction.commit();
-        } catch (Exception e) {
-            transaction.rollback();
-        }
+        zusatzwerte.add(wert1);
+        zusatzwerte.add(wert2);
+        zusatzwerte.add(wert3);
+        merger.mergeZusatzwerte(probe, zusatzwerte);
+        transaction.commit();
 
         shouldMatchDataSet(
             "datasets/dbUnit_import_merge_match_zusatzwert.xml",
@@ -437,31 +425,27 @@ public class ImporterTest extends BaseTest {
      */
     @Test
     public final void mergeProbeKommentar() throws Exception {
-        try {
-            transaction.begin();
-            Sample probe = repository.getByIdPlain(Sample.class, PID1000);
-            List<CommSample> kommentare = new ArrayList<CommSample>();
-            CommSample komm1 = new CommSample();
-            komm1.setSampleId(PID1000);
-            komm1.setDate(Timestamp.valueOf("2012-05-08 12:00:00"));
-            komm1.setMeasFacilId(mstId);
-            komm1.setText("Testtext2");
+        transaction.begin();
+        Sample probe = repository.getByIdPlain(Sample.class, PID1000);
+        List<CommSample> kommentare = new ArrayList<CommSample>();
+        CommSample komm1 = new CommSample();
+        komm1.setSampleId(PID1000);
+        komm1.setDate(Timestamp.valueOf("2012-05-08 12:00:00"));
+        komm1.setMeasFacilId(mstId);
+        komm1.setText("Testtext2");
 
-            CommSample komm2 = new CommSample();
-            komm2.setSampleId(PID1000);
-            komm2.setDate(Timestamp.valueOf("2012-04-08 12:00:00"));
-            komm2.setMeasFacilId(mstId);
-            komm2.setText("Testtext3");
+        CommSample komm2 = new CommSample();
+        komm2.setSampleId(PID1000);
+        komm2.setDate(Timestamp.valueOf("2012-04-08 12:00:00"));
+        komm2.setMeasFacilId(mstId);
+        komm2.setText("Testtext3");
 
-            kommentare.add(komm1);
-            kommentare.add(komm2);
+        kommentare.add(komm1);
+        kommentare.add(komm2);
 
-            merger.mergeKommentare(probe, kommentare);
-            Assert.assertEquals(2, kommentare.size());
-            transaction.commit();
-        } catch (Exception e) {
-            transaction.rollback();
-        }
+        merger.mergeKommentare(probe, kommentare);
+        Assert.assertEquals(2, kommentare.size());
+        transaction.commit();
 
         shouldMatchDataSet(
             "datasets/dbUnit_import_merge_match_kommentar.xml",
@@ -475,32 +459,28 @@ public class ImporterTest extends BaseTest {
      */
     @Test
     public final void mergeMessungKommentar() throws Exception {
-        try {
-            transaction.begin();
-            Measm messung =
-                repository.getByIdPlain(Measm.class, MID1200);
-            List<CommMeasm> kommentare = new ArrayList<CommMeasm>();
-            CommMeasm komm1 = new CommMeasm();
-            komm1.setMeasmId(MID1200);
-            komm1.setDate(Timestamp.valueOf("2012-05-08 12:00:00"));
-            komm1.setMeasFacilId(mstId);
-            komm1.setText("Testtext2");
+        transaction.begin();
+        Measm messung =
+            repository.getByIdPlain(Measm.class, MID1200);
+        List<CommMeasm> kommentare = new ArrayList<CommMeasm>();
+        CommMeasm komm1 = new CommMeasm();
+        komm1.setMeasmId(MID1200);
+        komm1.setDate(Timestamp.valueOf("2012-05-08 12:00:00"));
+        komm1.setMeasFacilId(mstId);
+        komm1.setText("Testtext2");
 
-            CommMeasm komm2 = new CommMeasm();
-            komm2.setMeasmId(MID1200);
-            komm2.setDate(Timestamp.valueOf("2012-03-08 12:00:00"));
-            komm2.setMeasFacilId(mstId);
-            komm2.setText("Testtext3");
+        CommMeasm komm2 = new CommMeasm();
+        komm2.setMeasmId(MID1200);
+        komm2.setDate(Timestamp.valueOf("2012-03-08 12:00:00"));
+        komm2.setMeasFacilId(mstId);
+        komm2.setText("Testtext3");
 
-            kommentare.add(komm1);
-            kommentare.add(komm2);
+        kommentare.add(komm1);
+        kommentare.add(komm2);
 
-            merger.mergeMessungKommentare(messung, kommentare);
-            Assert.assertEquals(2, kommentare.size());
-            transaction.commit();
-        } catch (Exception e) {
-            transaction.rollback();
-        }
+        merger.mergeMessungKommentare(messung, kommentare);
+        Assert.assertEquals(2, kommentare.size());
+        transaction.commit();
 
         shouldMatchDataSet(
             "datasets/dbUnit_import_merge_match_kommentarm.xml",
@@ -514,29 +494,25 @@ public class ImporterTest extends BaseTest {
      */
     @Test
     public final void mergeMesswerte() throws Exception {
-        try {
-            transaction.begin();
-            Measm messung =
-                repository.getByIdPlain(Measm.class, MID1200);
-            List<MeasVal> messwerte = new ArrayList<MeasVal>();
-            MeasVal wert1 = new MeasVal();
-            wert1.setMeasmId(MID1200);
-            wert1.setMeasUnitId(MEHID207);
-            wert1.setMeasdId(MGID56);
-            wert1.setMeasVal(MESS15D);
-            messwerte.add(wert1);
+        transaction.begin();
+        Measm messung =
+            repository.getByIdPlain(Measm.class, MID1200);
+        List<MeasVal> messwerte = new ArrayList<MeasVal>();
+        MeasVal wert1 = new MeasVal();
+        wert1.setMeasmId(MID1200);
+        wert1.setMeasUnitId(MEHID207);
+        wert1.setMeasdId(MGID56);
+        wert1.setMeasVal(MESS15D);
+        messwerte.add(wert1);
 
-            merger.mergeMesswerte(messung, messwerte);
-            QueryBuilder<MeasVal> builder =
-                repository.queryBuilder(MeasVal.class);
-            builder.and("measmId", messung.getId());
-            List<MeasVal> dbWerte =
-                repository.filterPlain(builder.getQuery());
-            Assert.assertEquals(1, dbWerte.size());
-            transaction.commit();
-        } catch (Exception e) {
-            transaction.rollback();
-        }
+        merger.mergeMesswerte(messung, messwerte);
+        QueryBuilder<MeasVal> builder =
+            repository.queryBuilder(MeasVal.class);
+        builder.and("measmId", messung.getId());
+        List<MeasVal> dbWerte =
+            repository.filterPlain(builder.getQuery());
+        Assert.assertEquals(1, dbWerte.size());
+        transaction.commit();
 
         shouldMatchDataSet(
             "datasets/dbUnit_import_merge_match_messwert.xml",
@@ -546,7 +522,6 @@ public class ImporterTest extends BaseTest {
 
     /**
      * Preliminary data for testing LAF 8 import.
-     * @throws Exception that can occur during the test.
      */
     @Test
     @RunAsClient
