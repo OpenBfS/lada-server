2f9fc662e7609ee13f960d383c7d998f3c9c128d
Alexander Woestmann <awoestmann@intevation.de>
2023-08-23 11:55:26 +0200

    Fix auto tag generation if run multiple times
    This fixes the query that is used to determine the last generated tag,
    which caused errors if the generation was run multiple times on the same
    day.

--- src/main/java/de/intevation/lada/util/data/TagUtil.java
+++ src/main/java/de/intevation/lada/util/data/TagUtil.java
@@ -63,7 +63,7 @@ public class TagUtil {
         CriteriaQuery<Tag> criteriaQuery = builder.createQuery(Tag.class);
         Root<Tag> tagRoot = criteriaQuery.from(Tag.class);
         Predicate nameFilter =
-            builder.like(tagRoot.get("name"), prefix + "\\_" + today + "\\_%");
+            builder.like(tagRoot.get("name"), prefix + "!_" + today + "!_%", '!');
         Order nameOrder = builder.asc(tagRoot.get("name"));
         criteriaQuery.where(nameFilter);
         criteriaQuery.orderBy(nameOrder);
