b6429f15f33faae55498f79052abf1386760faf8
Tom Gottfried <Tom Gottfried tom@intevation.de>
2023-10-18 16:28:48 +0200

    Simplify
    Also reduces the number of database round-trips from 2+3*n, with n
    being the number of requested measms, to 1.

--- src/main/java/de/intevation/lada/rest/StatusMpService.java
+++ src/main/java/de/intevation/lada/rest/StatusMpService.java
@@ -7,11 +7,7 @@
  */
 package de.intevation.lada.rest;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -19,14 +15,10 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
-import de.intevation.lada.model.lada.Measm;
-import de.intevation.lada.model.lada.StatusProt;
-import de.intevation.lada.model.master.StatusAccessMpView;
 import de.intevation.lada.model.master.StatusMp;
 import de.intevation.lada.util.annotation.AuthorizationConfig;
 import de.intevation.lada.util.auth.Authorization;
 import de.intevation.lada.util.auth.AuthorizationType;
-import de.intevation.lada.util.data.QueryBuilder;
 import de.intevation.lada.util.data.Repository;
 import de.intevation.lada.util.data.StatusCodes;
 import de.intevation.lada.util.rest.Response;
@@ -73,56 +65,27 @@ public class StatusMpService extends LadaService {
         return repository.getById(StatusMp.class, id);
     }
 
+    /**
+     * Get the union of status mappings that can be set on given measms.
+     *
+     * @param measmIds IDs of measms for which status mappings are requested
+     * @return Status mappings that can be set on given measms
+     */
     @POST
     @Path("getbyids")
     public Response getById(
-        List<Integer> messIds
+        List<Integer> measmIds
     ) {
-        QueryBuilder<Measm> messungQuery = repository
-            .queryBuilder(Measm.class)
-            .orIn("id", messIds);
-        List<Measm> messungen = repository.filterPlain(
-            messungQuery.getQuery());
-
-        Map<Integer, StatusAccessMpView> erreichbare =
-            new HashMap<Integer, StatusAccessMpView>();
-        for (Measm messung : messungen) {
-            StatusProt status = repository.getByIdPlain(
-                StatusProt.class, messung.getStatus());
-            StatusMp kombi = repository.getByIdPlain(
-                StatusMp.class, status.getStatusMpId());
-
-            QueryBuilder<StatusAccessMpView> errFilter = repository
-                .queryBuilder(StatusAccessMpView.class)
-                .andIn("statusLevId", authorization.getInfo().getFunktionen())
-                .and("curLevId", kombi.getStatusLev().getId())
-                .and("curValId", kombi.getStatusVal().getId());
-            List<StatusAccessMpView> err = repository.filterPlain(
-                errFilter.getQuery());
-            for (StatusAccessMpView e : err) {
-                erreichbare.put(e.getId(), e);
-            }
-        }
-
-        if (erreichbare.size() == 0) {
-            return new Response(
-                true, StatusCodes.OK, new ArrayList<StatusMp>());
-        }
-
-        QueryBuilder<StatusMp> kombiFilter =
-            repository.queryBuilder(StatusMp.class);
-        for (Entry<Integer, StatusAccessMpView> erreichbar
-            : erreichbare.entrySet()
-        ) {
-            QueryBuilder<StatusMp> tmp = kombiFilter.getEmptyBuilder()
-                .and("statusVal", erreichbar.getValue().getStatusValId())
-                .and("statusLev", erreichbar.getValue().getStatusLevId());
-            kombiFilter.or(tmp);
-        }
-
         return new Response(
             true,
             StatusCodes.OK,
-            repository.filterPlain(kombiFilter.getQuery()));
+            repository.entityManager().createNativeQuery(
+                "SELECT * FROM master.status_mp "
+                + "WHERE id IN(SELECT to_id FROM master.status_ord_mp "
+                + "  JOIN lada.measm ON from_id = status "
+                + "  WHERE measm.id IN(:measmIds))",
+                StatusMp.class)
+            .setParameter("measmIds", measmIds)
+            .getResultList());
     }
 }
