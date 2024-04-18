c62f6c72543c52d88b6f7591f3886ee2d6ec9bb4
Tom Gottfried <Tom Gottfried tom@intevation.de>
2024-04-18 11:28:50 +0200

    Fixup rev. eba89484
    Add missing class.

---
+++ src/main/java/de/intevation/lada/validation/groups/Notifications.java
@@ -0,0 +1,14 @@
+/* Copyright (C) 2024 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY! Check out
+ * the documentation coming with IMIS-Labordaten-Application for details.
+ */
+
+package de.intevation.lada.validation.groups;
+
+/**
+ * Marker interface for grouping bean validation constraints.
+ */
+public interface Notifications { }
