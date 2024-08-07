OpenBfS/lada-server

commit 0936f3a7daa2477bc513953deaf4d7490ac68acc

Move status validation from service to Bean Validation constraint
Also separates test data for status validation testing to avoid
interference with other tests.

src/main/java/de/intevation/lada/model/lada/StatusProt.java
@@ -29,12 +29,14 @@ import de.intevation.lada.model.master.MeasFacil;
 import de.intevation.lada.model.master.StatusMp;
 import de.intevation.lada.validation.constraints.IsValidPrimaryKey;
 import de.intevation.lada.validation.constraints.NotEmptyNorWhitespace;
+import de.intevation.lada.validation.constraints.ValidDependencies;
 import de.intevation.lada.validation.groups.DatabaseConstraints;
 
 
 @Entity
 @Table(schema = SchemaName.NAME)
 @GroupSequence({ StatusProt.class, DatabaseConstraints.class })
+@ValidDependencies(groups = DatabaseConstraints.class)
 public class StatusProt extends BaseModel implements Serializable {
     private static final long serialVersionUID = 1L;
 

commit 40b575d6ef64185d419df4c6225d6db40130336a

Re-add notifications to response after successfully setting status
Fixes a regression of rev. 0936f3a7, which accidentally removed these
notifications. Avoid usage of instance variables in constraint validator
base class because ConstraintValidator.isValid() can be accessed concurrently.

src/main/java/de/intevation/lada/model/lada/StatusProt.java
@@ -30,14 +30,17 @@ import de.intevation.lada.model.master.StatusMp;
 import de.intevation.lada.validation.constraints.IsValidPrimaryKey;
 import de.intevation.lada.validation.constraints.NotEmptyNorWhitespace;
 import de.intevation.lada.validation.constraints.StatusOrder;
-import de.intevation.lada.validation.constraints.ValidDependencies;
+import de.intevation.lada.validation.constraints.HaveDependenciesNotifications;
+import de.intevation.lada.validation.constraints.ValidDependenciesFinalStatus;
 import de.intevation.lada.validation.groups.DatabaseConstraints;
+import de.intevation.lada.validation.groups.Notifications;
 
 
 @Entity
 @Table(schema = SchemaName.NAME)
 @GroupSequence({ StatusProt.class, DatabaseConstraints.class })
-@ValidDependencies(groups = DatabaseConstraints.class)
+@ValidDependenciesFinalStatus(groups = DatabaseConstraints.class)
+@HaveDependenciesNotifications(groups = Notifications.class)
 @StatusOrder(groups = DatabaseConstraints.class)
 public class StatusProt extends BaseModel implements Serializable {
     private static final long serialVersionUID = 1L;

src/main/java/de/intevation/lada/validation/constraints/HaveDependenciesNotifications.java
@@ -19,15 +19,17 @@ import jakarta.validation.Payload;
 
 /**
  * Validation rule for status.
+ * A StatusProt is considered invalid for certain statusMpIds, if validation
+ * of any related Sample, Measm, MeasVal or Site returns notificactions.
  */
 @Target({ TYPE })
 @Retention(RUNTIME)
-@Constraint(validatedBy = { ValidDependenciesValidator.class })
+@Constraint(validatedBy = { HaveDependenciesNotificationsValidator.class })
 @Documented
-public @interface ValidDependencies {
+public @interface HaveDependenciesNotifications {
 
     static final String MSG =
-        "{de.intevation.lada.validation.constraints.ValidDependencies.message}";
+        "{de.intevation.lada.validation.constraints.HaveDependenciesNotifications.message}";
 
     String message() default MSG;
 

src/main/java/de/intevation/lada/validation/constraints/HaveDependenciesNotificationsValidator.java
@@ -1,0 +1,40 @@
+/* Copyright (C) 2024 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY! Check out
+ * the documentation coming with IMIS-Labordaten-Application for details.
+ */
+package de.intevation.lada.validation.constraints;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import de.intevation.lada.model.lada.StatusProt;
+
+
+/**
+ * Validation rule for status.
+ * A StatusProt is considered invalid for certain statusMpIds, if validation
+ * of any related Sample, Measm, MeasVal or Site returns notificactions.
+ */
+public class HaveDependenciesNotificationsValidator
+    extends ValidDependenciesValidator
+    implements ConstraintValidator<HaveDependenciesNotifications, StatusProt> {
+
+    private String message;
+
+    @Override
+    public void initialize(HaveDependenciesNotifications constraintAnnotation) {
+        this.message = constraintAnnotation.message();
+    }
+
+    @Transactional
+    @Override
+    public boolean isValid(
+        StatusProt status, ConstraintValidatorContext ctx
+    ) {
+        return doValidation(status, ctx, this.message, true);
+    }
+}

src/main/java/de/intevation/lada/validation/constraints/ValidDependenciesFinalStatus.java
@@ -1,0 +1,37 @@
+/* Copyright (C) 2024 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY! Check out
+ * the documentation coming with IMIS-Labordaten-Application for details.
+ */
+package de.intevation.lada.validation.constraints;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+/**
+ * Validation rule for status.
+ */
+@Target({ TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = { ValidDependenciesFinalStatusValidator.class })
+@Documented
+public @interface ValidDependenciesFinalStatus {
+
+    static final String MSG =
+        "{de.intevation.lada.validation.constraints.ValidDependenciesFinalStatus.message}";
+
+    String message() default MSG;
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+}

src/main/java/de/intevation/lada/validation/constraints/ValidDependenciesFinalStatusValidator.java
@@ -1,0 +1,38 @@
+/* Copyright (C) 2024 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY! Check out
+ * the documentation coming with IMIS-Labordaten-Application for details.
+ */
+package de.intevation.lada.validation.constraints;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import de.intevation.lada.model.lada.StatusProt;
+
+
+/**
+ * Validation rule for status.
+ */
+public class ValidDependenciesFinalStatusValidator
+    extends ValidDependenciesValidator
+    implements ConstraintValidator<ValidDependenciesFinalStatus, StatusProt> {
+
+    private String message;
+
+    @Override
+    public void initialize(ValidDependenciesFinalStatus constraintAnnotation) {
+        this.message = constraintAnnotation.message();
+    }
+
+    @Transactional
+    @Override
+    public boolean isValid(
+        StatusProt status, ConstraintValidatorContext ctx
+    ) {
+        return doValidation(status, ctx, this.message, false);
+    }
+}

src/main/java/de/intevation/lada/validation/constraints/ValidDependenciesValidator.java
@@ -15,8 +15,6 @@ import java.util.Set;
 
 import jakarta.enterprise.inject.Instance;
 import jakarta.enterprise.inject.spi.CDI;
-import jakarta.transaction.Transactional;
-import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 import org.hibernate.validator.constraintvalidation.HibernateConstraintValidatorContext;
@@ -41,29 +39,14 @@ import de.intevation.lada.validation.Validator;
 /**
  * Validation rule for status.
  */
-public class ValidDependenciesValidator
-    implements ConstraintValidator<ValidDependencies, StatusProt> {
+public abstract class ValidDependenciesValidator {
 
-    private String message;
-
-    private Repository repository;
-
-    private Validator validator;
-
-    private Map<String, Set<String>> errors = new HashMap<>();
-
-    private Map<String, Set<String>> warnings = new HashMap<>();
-
-    private Map<String, Set<String>> notifications = new HashMap<>();
-
-    @Override
-    public void initialize(ValidDependencies constraintAnnotation) {
-        this.message = constraintAnnotation.message();
-    }
-
-    @Override
-    @Transactional
-    public boolean isValid(StatusProt status, ConstraintValidatorContext ctx) {
+    protected boolean doValidation(
+        StatusProt status,
+        ConstraintValidatorContext ctx,
+        String message,
+        boolean checkNotifications
+    ) {
         if (status == null
             || status.getStatusMpId() == null
             || status.getMeasmId() == null
@@ -75,8 +58,8 @@ public class ValidDependenciesValidator
         // is not guaranteed to work in ConstraintValidator implementations
         Instance<Object> inst =
             CDI.current().getBeanContainer().createInstance();
-        this.repository = inst.select(Repository.class).get();
-        this.validator = inst.select(Validator.class).get();
+        Repository repository = inst.select(Repository.class).get();
+        Validator validator = inst.select(Validator.class).get();
 
         StatusMp newKombi = repository.entityManager().find(
             StatusMp.class, status.getStatusMpId());
@@ -94,13 +77,19 @@ public class ValidDependenciesValidator
             Sample probe = repository.getById(
                 Sample.class, messung.getSampleId());
 
-            // init violation_collection with probe validation
-            addMessages(validator.validate(probe));
+            Map<String, Set<String>> errors = new HashMap<>();
+            Map<String, Set<String>> warnings = new HashMap<>();
+            Map<String, Set<String>> notifications = new HashMap<>();
 
-            //validate messung object
-            addMessages(validator.validate(messung));
+            // Validate sample
+            addMessages(
+                validator.validate(probe), errors, warnings, notifications);
 
-            //validate messwert objects
+            // Validate Measm
+            addMessages(
+                validator.validate(messung), errors, warnings, notifications);
+
+            // Validate measVals
             QueryBuilder<MeasVal> builder = repository
                 .queryBuilder(MeasVal.class)
                 .and(MeasVal_.measmId, messung.getId());
@@ -112,16 +101,20 @@ public class ValidDependenciesValidator
                         && !(messwert.getMeasVal() == null
                             && messwert.getLessThanLOD() == null)
                     ) {
-                        addError("status", StatusCodes.STATUS_RO);
+                        addError("status", StatusCodes.STATUS_RO, errors);
                     }
 
-                    addMessages(validator.validate(messwert));
+                    addMessages(
+                        validator.validate(messwert),
+                        errors,
+                        warnings,
+                        notifications);
                 }
             } else if (newStatusWert != 7) {
-                addError("measVal", StatusCodes.VALUE_MISSING);
+                addError("measVal", StatusCodes.VALUE_MISSING, errors);
             }
 
-            // validate orte
+            // Validate sites
             QueryBuilder<Geolocat> ortBuilder = repository
                 .queryBuilder(Geolocat.class)
                 .and(Geolocat_.sampleId, probe.getId());
@@ -129,42 +122,55 @@ public class ValidDependenciesValidator
                 ortBuilder.getQuery());
             for (Geolocat o : assignedOrte) {
                 Site site = repository.getById(Site.class, o.getSiteId());
-                addMessages(validator.validate(site));
+                addMessages(
+                    validator.validate(site),
+                    errors,
+                    warnings,
+                    notifications);
             }
 
-            if (newStatusWert != 7
-                && (!this.errors.isEmpty() || !this.warnings.isEmpty())
-                || newStatusWert == 7
-                && (probe.hasErrors() || probe.hasWarnings())
-            ) {
-                HibernateConstraintValidatorContext hibernateCtx = ctx.unwrap(
-                    HibernateConstraintValidatorContext.class
-                );
-                hibernateCtx.disableDefaultConstraintViolation();
-                hibernateCtx.addExpressionVariable("errs", this.errors)
-                    .addExpressionVariable("wrns", this.warnings)
-                    .addExpressionVariable("nots", this.notifications)
-                    .buildConstraintViolationWithTemplate(this.message)
-                    .enableExpressionLanguage(BEAN_METHODS)
-                    .addPropertyNode("status")
-                    .addConstraintViolation();
-                return false;
+            HibernateConstraintValidatorContext hibernateCtx = ctx.unwrap(
+                HibernateConstraintValidatorContext.class
+            );
+            hibernateCtx.disableDefaultConstraintViolation();
+            hibernateCtx.addExpressionVariable("errs", errors)
+                .addExpressionVariable("wrns", warnings)
+                .addExpressionVariable("nots", notifications)
+                .buildConstraintViolationWithTemplate(message)
+                .enableExpressionLanguage(BEAN_METHODS)
+                .addPropertyNode("status")
+                .addConstraintViolation();
+
+            if (checkNotifications) {
+                return notifications.isEmpty();
             }
+
+            return !(newStatusWert != 7
+                && (!errors.isEmpty() || !warnings.isEmpty())
+                || newStatusWert == 7
+                && (probe.hasErrors() || probe.hasWarnings()));
         }
         return true;
     }
 
-    private void addError(String key, int value) {
-        if (!this.errors.containsKey(key)) {
-            this.errors.put(key, new HashSet<String>());
+    private void addError(
+        String key, int value, Map<String, Set<String>> errors
+    ) {
+        if (!errors.containsKey(key)) {
+            errors.put(key, new HashSet<String>());
         }
-        this.errors.get(key).add(String.valueOf(value));
+        errors.get(key).add(String.valueOf(value));
     }
 
-    private void addMessages(BaseModel validated) {
-        addMessages(validated.getErrors(), this.errors);
-        addMessages(validated.getWarnings(), this.warnings);
-        addMessages(validated.getNotifications(), this.notifications);
+    private void addMessages(
+        BaseModel validated,
+        Map<String, Set<String>> errors,
+        Map<String, Set<String>> warnings,
+        Map<String, Set<String>> notifications
+    ) {
+        addMessages(validated.getErrors(), errors);
+        addMessages(validated.getWarnings(), warnings);
+        addMessages(validated.getNotifications(), notifications);
     }
 
     private void addMessages(

src/main/resources/ValidationMessages.properties
@@ -9,7 +9,8 @@ de.intevation.lada.validation.constraints.HasCoordsOrAdminUnitOrState.message=Ei
 de.intevation.lada.validation.constraints.SupportedSpatRefSysId.message=Spatial reference system not supported
 de.intevation.lada.validation.GeolocatUniqueSamplingLocation=Sampling location (typeRegulation "E" or "R") must be unique
 de.intevation.lada.validation.constraints.ValidCoordinates.message=The given coordinates are invalid
-de.intevation.lada.validation.constraints.ValidDependencies.message=Operation not possible due to constraint violations${empty errs ? '' : '\nErrors:\n'+=errs.keySet().stream().map(k->'- '+=k+=': '+=errs.get(k)).reduce((a,b)->a+='\n'+=b).orElse('')}${empty wrns ? '' : '\nWarnings:\n'+=wrns.keySet().stream().map(k->'- '+=k+=': '+=wrns.get(k)).reduce((a,b)->a+='\n'+=b).orElse('')}${empty nots ? '' : '\nNotifications:\n'+=nots.keySet().stream().map(k->'- '+=k+=': '+=nots.get(k)).reduce((a,b)->a+='\n'+=b).orElse('')}
+de.intevation.lada.validation.constraints.HaveDependenciesNotifications.message=${'Notifications:\n'+=nots.keySet().stream().map(k->'- '+=k+=': '+=nots.get(k)).reduce((a,b)->a+='\n'+=b).orElse('')}
+de.intevation.lada.validation.constraints.ValidDependenciesFinalStatus.message=Operation not possible due to constraint violations${empty errs ? '' : '\nErrors:\n'+=errs.keySet().stream().map(k->'- '+=k+=': '+=errs.get(k)).reduce((a,b)->a+='\n'+=b).orElse('')}${empty wrns ? '' : '\nWarnings:\n'+=wrns.keySet().stream().map(k->'- '+=k+=': '+=wrns.get(k)).reduce((a,b)->a+='\n'+=b).orElse('')}${empty nots ? '' : '\nNotifications:\n'+=nots.keySet().stream().map(k->'- '+=k+=': '+=nots.get(k)).reduce((a,b)->a+='\n'+=b).orElse('')}
 de.intevation.lada.validation.constraints.DatesVsSampleMeth.message=Individual sample expects sample start date = sample end date
 de.intevation.lada.validation.constraints.OrigDateVsStartDate.message=Time of origin must be before or equal to sampling time
 de.intevation.lada.validation.constraints.MeasuringAfterSampling.message=Values do not match

src/main/resources/ValidationMessages_de.properties
@@ -9,7 +9,8 @@ de.intevation.lada.validation.constraints.HasCoordsOrAdminUnitOrState.message=En
 de.intevation.lada.validation.constraints.SupportedSpatRefSysId.message=Koordinatenart wird nicht unterstützt
 de.intevation.lada.validation.GeolocatUniqueSamplingLocation=Es existiert bereits ein Entnahmeort
 de.intevation.lada.validation.constraints.ValidCoordinates.message=Die angegebenen Koordinaten sind ungültig
-de.intevation.lada.validation.constraints.ValidDependencies.message=Die Operation ist wegen verschiedener Konsistenzverletzungen nicht möglich${empty errs ? '' : '\nFehler:\n'+=errs.keySet().stream().map(k->'- '+=k+=': '+=errs.get(k)).reduce((a,b)->a+='\n'+=b).orElse('')}${empty wrns ? '' : '\nWarnungen:\n'+=wrns.keySet().stream().map(k->'- '+=k+=': '+=wrns.get(k)).reduce((a,b)->a+='\n'+=b).orElse('')}${empty nots ? '' : '\nHinweismeldungen:\n'+=nots.keySet().stream().map(k->'- '+=k+=': '+=nots.get(k)).reduce((a,b)->a+='\n'+=b).orElse('')}
+de.intevation.lada.validation.constraints.HaveDependenciesNotifications.message=${'Hinweismeldungen:\n'+=nots.keySet().stream().map(k->'- '+=k+=': '+=nots.get(k)).reduce((a,b)->a+='\n'+=b).orElse('')}
+de.intevation.lada.validation.constraints.ValidDependenciesFinalStatus.message=Die Operation ist wegen verschiedener Konsistenzverletzungen nicht möglich${empty errs ? '' : '\nFehler:\n'+=errs.keySet().stream().map(k->'- '+=k+=': '+=errs.get(k)).reduce((a,b)->a+='\n'+=b).orElse('')}${empty wrns ? '' : '\nWarnungen:\n'+=wrns.keySet().stream().map(k->'- '+=k+=': '+=wrns.get(k)).reduce((a,b)->a+='\n'+=b).orElse('')}${empty nots ? '' : '\nHinweismeldungen:\n'+=nots.keySet().stream().map(k->'- '+=k+=': '+=nots.get(k)).reduce((a,b)->a+='\n'+=b).orElse('')}
 de.intevation.lada.validation.constraints.DatesVsSampleMeth.message=Einzelprobe erwartet Probeentnahme-Ende = Probeentnahme-Beginn
 de.intevation.lada.validation.constraints.OrigDateVsStartDate.message=Ursprungszeit muss vor oder gleich der Probeentnahmezeit sein
 de.intevation.lada.validation.constraints.MeasuringAfterSampling.message=Werte passen nicht zusammen

src/test/java/de/intevation/lada/test/validator/StatusTest.java
@@ -31,6 +31,8 @@ public class StatusTest extends ValidatorBaseTest {
 
     private static final int INVALID_STATUS_MP_ID = 42;
 
+    private static final String MSG_KEY = "status";
+
     @Inject
     private Validator validator;
 
@@ -123,7 +125,7 @@ public class StatusTest extends ValidatorBaseTest {
         status.setMeasmId(invalidMeasmId);
         assertHasErrors(
             validator.validate(status),
-            "status",
+            MSG_KEY,
             "Operation not possible due to constraint violations\n"
             + "Errors:\n"
             + "- measVal: [631]\n"
@@ -145,6 +147,41 @@ public class StatusTest extends ValidatorBaseTest {
         status.setStatusMpId(notPlausible);
         status.setMeasmId(invalidMeasmId);
         assertNoMessages(validator.validate(status));
+    }
+
+    /**
+     * Test status of measm with warning and notification at measm.
+     */
+    @Test
+    public void measmWithWarnings() {
+        StatusProt status = minimalStatusProt();
+        final int invalidMeasmId = 1202;
+        status.setMeasmId(invalidMeasmId);
+        assertHasErrors(
+            validator.validate(status),
+            MSG_KEY,
+            "Operation not possible due to constraint violations\n"
+            + "Warnings:\n"
+            + "- measmStartDate: [A value must be provided]\n"
+            + "Notifications:\n"
+            + "- minSampleId: [must not be blank]"
+        );
+    }
+
+    /**
+     * Test status of measm with notification at measm.
+     */
+    @Test
+    public void measmWithNotifications() {
+        StatusProt status = minimalStatusProt();
+        final int measmId = 1203;
+        status.setMeasmId(measmId);
+        assertHasNotifications(
+            validator.validate(status),
+            MSG_KEY,
+            "Notifications:\n"
+            + "- minSampleId: [must not be blank]"
+        );
     }
 
     private StatusProt minimalStatusProt() {

src/test/resources/datasets/dbUnit_status_validator.xml
@@ -155,6 +155,49 @@
         status_mp_id = "1"
     />
 
+    <!-- measm with warning because of missing measmStartDate,
+         notification because of missing minSampleId -->
+    <lada.measm
+        id = "1202"
+        sample_id = "1000"
+        ext_id = "455"
+        mmt_id = "A3"
+        meas_pd = "3600"
+        is_completed = "false"
+        is_scheduled = "true"
+    />
+    <lada.meas_val
+        id = "10002"
+        measm_id = "1202"
+        measd_id = "56"
+        meas_val = "42.0"
+        error = "3.3"
+        detect_lim = "2.4"
+        meas_unit_id = "207"
+    />
+
+    <!-- measm with notification because of missing minSampleId -->
+    <lada.measm
+        id = "1203"
+        sample_id = "1000"
+        ext_id = "456"
+        mmt_id = "A3"
+        meas_pd = "3600"
+        is_completed = "false"
+        is_scheduled = "true"
+        measm_start_date = "2012-05-06 14:00:00"
+    />
+    <lada.meas_val
+        id = "10003"
+        measm_id = "1203"
+        measd_id = "56"
+        meas_val = "42.0"
+        error = "3.3"
+        detect_lim = "2.4"
+        meas_unit_id = "207"
+    />
+
+
     <!--Valid REI sample-->
     <lada.sample
         id = "4000"