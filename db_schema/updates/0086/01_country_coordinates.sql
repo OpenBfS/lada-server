cc9e615ed5020cd7c014b3a950bd56b74ec34fe3
Arne Schumacher <aschumacher@bfs.de>
2024-08-14 15:45:21 +0200

    correct typo

--- db_schema/updates/0086/01_country_coordinates.sql
+++ db_schema/updates/0086/01_country_coordinates.sql
@@ -2,4 +2,4 @@ UPDATE master.site SET coord_x_ext="32,5", coord_y_ext="15,6", spat_ref_sys_id=4
 UPDATE master.site SET coord_x_ext="31,6", coord_y_ext="4,85", spat_ref_sys_id=4 WHERE id=685;
 UPDATE master.site SET coord_x_ext="0", coord_y_ext="0", spat_ref_sys_id=4 WHERE id=683;
 ALTER TABLE master.site ALTER COLUMN coord_x_ext SET NOT NULL;
-ALTER TABLE master.site ALTER COLUMN coord_x_ext SET NOT NULL;
+ALTER TABLE master.site ALTER COLUMN coord_y_ext SET NOT NULL;
