43aac6177498e6b6d25c7c28c103a228e9cc4cf1
Alexander Woestmann <awoestmann@intevation.de>
2024-08-14 11:27:02 +0200

    Fix IsValidCharsetString annotation target

--- src/main/java/de/intevation/lada/validation/constraints/requests/IsValidCharsetString.java
+++ src/main/java/de/intevation/lada/validation/constraints/requests/IsValidCharsetString.java
@@ -7,9 +7,7 @@
  */
 package de.intevation.lada.validation.constraints.requests;
 
-import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.TYPE_USE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
@@ -19,8 +17,7 @@ import java.lang.annotation.Target;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
-
-@Target({ FIELD, ANNOTATION_TYPE, TYPE_USE })
+@Target({ FIELD})
 @Retention(RUNTIME)
 @Constraint(validatedBy = {
     IsValidCharsetStringValidator.class
