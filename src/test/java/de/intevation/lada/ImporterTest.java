19c3a3ef630201d853d860817a55234c5efb6d17
Tom Gottfried <tom@intevation.de>
2025-06-11 09:56:00 +0200

    Separate unit and integration tests of import logic

--- src/test/java/de/intevation/lada/ImporterTest.java
+++ src/test/java/de/intevation/lada/ImporterTest.java
@@ -9,10 +9,8 @@ package de.intevation.lada;
 
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.StandardCharsets;
-import java.sql.Timestamp;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 import java.util.Locale;
@@ -20,13 +18,10 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.UUID;
 
-import jakarta.annotation.Resource;
-import jakarta.inject.Inject;
 import jakarta.json.Json;
 import jakarta.json.JsonArray;
 import jakarta.json.JsonObject;
 import jakarta.json.JsonValue;
-import jakarta.transaction.UserTransaction;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.client.SyncInvoker;
 import jakarta.ws.rs.core.GenericType;
@@ -37,27 +32,20 @@ import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.transaction.api.annotation.Transactional;
 import org.jboss.logging.Logger;
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import de.intevation.lada.rest.AsyncLadaService.AsyncJobResponse;
 import de.intevation.lada.data.requests.LafImportParameters;
-import de.intevation.lada.importer.Identifier;
-import de.intevation.lada.importer.ObjectMerger;
 import de.intevation.lada.model.lada.MeasVal;
-import de.intevation.lada.model.lada.MeasVal_;
 import de.intevation.lada.model.lada.Measm;
 import de.intevation.lada.model.lada.Sample;
 import de.intevation.lada.model.lada.SampleSpecifMeasVal;
 import de.intevation.lada.util.data.Job;
 import de.intevation.lada.util.data.Job.JobStatus;
-import de.intevation.lada.util.data.QueryBuilder;
-import de.intevation.lada.util.data.Repository;
 import de.intevation.lada.util.data.StatusCodes;
 
 
@@ -72,21 +60,6 @@ public class ImporterTest extends BaseTest {
     private static final Logger LOG = Logger.getLogger(ImporterTest.class);
 
     private static final String ASYNC_IMPORT_URL = "data/import/async/";
-
-    private static final double MESS15D = 1.5d;
-    private static final int MGID56 = 56;
-    private static final int MEHID207 = 207;
-    private static final double MESS18D = 1.8d;
-    private static final float MESSFEHLER02F = 0.2f;
-    private static final float MESSFEHLER12F = 1.2f;
-    private static final int MDAUER1000 = 1000;
-    private static final int MID1200 = 1200;
-    private static final int PNID = 726;
-    private static final int MPRID1000 = 1000;
-    private static final int PID1000 = 1000;
-    private static final Integer DID9 = 9;
-
-    @Resource UserTransaction transaction;
 
     private final String mstId = "06010";
     private final String regulation = "test";
@@ -111,18 +84,6 @@ public class ImporterTest extends BaseTest {
         + "%s"
         + "%%ENDE%%\n";
 
-    @Inject
-    Identifier<Sample> probeIdentifier;
-
-    @Inject
-    Identifier<Measm> messungIdentifier;
-
-    @Inject
-    Repository repository;
-
-    @Inject
-    ObjectMerger merger;
-
     public ImporterTest() {
         testDatasetName = "datasets/dbUnit_import.xml";
     }
@@ -137,317 +98,6 @@ public class ImporterTest extends BaseTest {
             .header("X-SHIB-user", BaseTest.testUser)
             .header("X-SHIB-roles", BaseTest.testRoles)
             .get();
-    }
-
-    /**
-     * Identify probe objects.
-     *
-     * @throws Exception that can occur during the test.
-     */
-    @Test
-    @Transactional
-    public final void identifyProbeByHPNrMST() throws Exception {
-        Sample probe = new Sample();
-        probe.setMainSampleId("120510002");
-        probe.setMeasFacilId(mstId);
-        probe.setIsTest(false);
-
-        Assert.assertNotNull(probeIdentifier.getExisting(probe));
-    }
-
-    /**
-     * Identify probject by HP-Nr and MST.
-     * @throws Exception that can occur during the test.
-     */
-    @Test
-    @Transactional
-    public final void identifyProbeByHPNrMSTNew() throws Exception {
-        Sample probe = new Sample();
-        probe.setMainSampleId("120510003");
-        probe.setMeasFacilId(mstId);
-
-        Assert.assertNull(probeIdentifier.getExisting(probe));
-    }
-
-    /**
-     * Identify probe object by external probe id.
-     *
-     * @throws Exception that can occur during the test.
-     */
-    @Test
-    @Transactional
-    public final void identifyProbeByExterneProbeId() throws Exception {
-        Sample probe = new Sample();
-        probe.setExtId("T001");
-
-        Assert.assertNotNull(probeIdentifier.getExisting(probe));
-    }
-
-    /**
-     * Identify probe object by external id as new.
-     * @throws Exception that can occur during test.
-     */
-    @Test
-    @Transactional
-    public final void identifyProbeByExterneProbeIdNew() throws Exception {
-        Sample probe = new Sample();
-        probe.setExtId("T002");
-
-        Assert.assertNull(probeIdentifier.getExisting(probe));
-    }
-
-    /**
-     * Identify probe object by external id as reject.
-     * @throws Exception that can occur during the test.
-     */
-    @Test
-    @Transactional
-    public final void identifyProbeByExterneProbeIdReject() throws Exception {
-        Sample probe = new Sample();
-        probe.setExtId("T001");
-        probe.setMainSampleId("120510003");
-        probe.setMeasFacilId(mstId);
-
-        Assert.assertThrows(
-            Identifier.IdentificationException.class,
-            () -> probeIdentifier.getExisting(probe));
-    }
-
-    /**
-     * Identify probe object by external id as update.
-     * @throws Exception that ca occur during the test.
-     */
-    @Test
-    @Transactional
-    public final void identifyProbeByExterneProbeIdUpdate() throws Exception {
-        Sample probe = new Sample();
-        probe.setExtId("T001");
-        probe.setMainSampleId("");
-        probe.setMeasFacilId(mstId);
-
-        Assert.assertNotNull(probeIdentifier.getExisting(probe));
-    }
-
-    /**
-     * Identify messung object by np nr.
-     * @throws Exception that can occur during the test.
-     */
-    @Test
-    @Transactional
-    public final void identifyMessungByNpNr() throws Exception {
-        Measm messung = new Measm();
-        messung.setSampleId(PID1000);
-        messung.setMinSampleId("06A0");
-
-        Assert.assertNotNull(messungIdentifier.getExisting(messung));
-    }
-
-    /**
-     * Identify messung object by np nr. as new.
-     * @throws Exception that can occur during the test.
-     */
-    @Test
-    @Transactional
-    public final void identifyMessungByNpNrNew() throws Exception {
-        Measm messung = new Measm();
-        messung.setSampleId(PID1000);
-        messung.setMinSampleId("06A1");
-
-        Assert.assertNull(messungIdentifier.getExisting(messung));
-    }
-
-    /**
-     * Identify messung object by external id.
-     * @throws Exception that can occur during the test.
-     */
-    @Test
-    @Transactional
-    public final void identifyMessungByExterneMessungsId() throws Exception {
-        Measm messung = new Measm();
-        messung.setSampleId(PID1000);
-        messung.setExtId(1);
-
-        Assert.assertNotNull(messungIdentifier.getExisting(messung));
-    }
-
-    /**
-     * Identify messung object by external id as new.
-     * @throws Exception that can occur during the test.
-     */
-    @Test
-    @Transactional
-    public final void identifyMessungByExterneMessungsIdNew() throws Exception {
-        Measm messung = new Measm();
-        messung.setSampleId(PID1000);
-        messung.setExtId(2);
-
-        Assert.assertNull(messungIdentifier.getExisting(messung));
-    }
-
-    /**
-     * Unidentifiable Measm object.
-     * @throws Exception that can occur during the test.
-     */
-    @Test
-    @Transactional
-    public final void identifyMessungReject()
-    throws Exception {
-        Measm messung = new Measm();
-        messung.setSampleId(PID1000);
-
-        Assert.assertThrows(
-            Identifier.IdentificationException.class,
-            () -> messungIdentifier.getExisting(messung));
-    }
-
-    /**
-     * Identify messung object by external id as update.
-     * @throws Exception that can occur during the test.
-     */
-    @Test
-    @Transactional
-    public final void identifyMessungByExterneMessungsIdUpdate()
-    throws Exception {
-        Measm messung = new Measm();
-        messung.setSampleId(PID1000);
-        messung.setExtId(1);
-        messung.setMinSampleId("");
-
-        Assert.assertNotNull(messungIdentifier.getExisting(messung));
-    }
-
-    /**
-     * Merge probe objects.
-     * @throws Exception that can occur during the test.
-     */
-    @Test
-    public final void mergeProbe() throws Exception {
-        transaction.begin();
-        Sample probe = new Sample();
-        probe.setExtId("T001");
-        probe.setMainSampleId("120510002");
-        probe.setMeasFacilId(mstId);
-        probe.setOprModeId(1);
-        probe.setRegulationId(DID9);
-        probe.setEnvDescripName(
-            "Trinkwasser Zentralversorgung Oberflächenwasser aufbereitet");
-        probe.setEnvDescripDisplay("D: 59 04 01 00 05 05 01 02 00 00 00 00");
-        probe.setMpgId(MPRID1000);
-        probe.setSamplerId(PNID);
-        probe.setIsTest(false);
-        probe.setApprLabId(mstId);
-        probe.setSampleMethId(2);
-        probe.setEnvMediumId("A6");
-        probe.setSchedStartDate(Timestamp.valueOf("2013-05-01 16:00:00"));
-        probe.setSchedEndDate(Timestamp.valueOf("2013-05-05 16:00:00"));
-        probe.setSampleStartDate(Timestamp.valueOf("2012-05-03 13:07:00"));
-        Sample dbProbe = repository.getById(Sample.class, PID1000);
-        merger.merge(dbProbe, probe);
-        transaction.commit();
-
-        shouldMatchDataSet("datasets/dbUnit_import_merge_match.xml",
-            "lada.sample", new String[]{"last_mod", "tree_mod", "mid_coll_pd"});
-    }
-
-    /**
-     * Merge messung objects.
-     * @throws Exception that can occur during the test
-     */
-    @Test
-    public final void mergeMessung() throws Exception {
-        transaction.begin();
-        Measm messung = new Measm();
-        messung.setMinSampleId("06A0");
-        messung.setIsScheduled(true);
-        messung.setIsCompleted(false);
-        messung.setMeasPd(MDAUER1000);
-        messung.setMmtId("A3");
-        messung.setMeasmStartDate(Timestamp.valueOf("2012-05-06 14:00:00"));
-        Measm dbMessung =
-            repository.getById(Measm.class, MID1200);
-        merger.mergeMessung(dbMessung, messung);
-        transaction.commit();
-
-        shouldMatchDataSet(
-            "datasets/dbUnit_import_merge_match_messung.xml",
-            "lada.measm",
-            new String[]{"status", "last_mod", "tree_mod"});
-    }
-
-    // TODO Record order can get mixed up here which cause the test to fail as
-    //       different records get compared to each other (e.g. A74 <-> A76)
-    /**
-     * Merge zusatzwert objects.
-     * @throws Exception that can occur during the test.
-     */
-    @Test
-    @Ignore
-    public final void mergeZusatzwert() throws Exception {
-        transaction.begin();
-        Sample probe = repository.getById(Sample.class, PID1000);
-        List<SampleSpecifMeasVal> zusatzwerte = new ArrayList<SampleSpecifMeasVal>();
-        SampleSpecifMeasVal wert1 = new SampleSpecifMeasVal();
-        wert1.setSampleId(PID1000);
-        wert1.setError(MESSFEHLER12F);
-        wert1.setSmallerThan("<");
-        wert1.setSampleSpecifId("A74");
-
-        SampleSpecifMeasVal wert2 = new SampleSpecifMeasVal();
-        wert2.setSampleId(PID1000);
-        wert2.setError(MESSFEHLER02F);
-        wert2.setMeasVal(MESS18D);
-        wert1.setSmallerThan(null);
-        wert2.setSampleSpecifId("A75");
-
-        SampleSpecifMeasVal wert3 = new SampleSpecifMeasVal();
-        wert3.setSampleId(PID1000);
-        wert3.setError(MESSFEHLER02F);
-        wert3.setMeasVal(MESS18D);
-        wert1.setSmallerThan(null);
-        wert3.setSampleSpecifId("A76");
-
-        zusatzwerte.add(wert1);
-        zusatzwerte.add(wert2);
-        zusatzwerte.add(wert3);
-        merger.mergeZusatzwerte(probe, zusatzwerte);
-        transaction.commit();
-
-        shouldMatchDataSet(
-            "datasets/dbUnit_import_merge_match_zusatzwert.xml",
-            "lada.sample_specif_meas_val",
-            new String[]{"id", "last_mod", "tree_mod"});
-    }
-
-    /**
-     * Merge messwert objects.
-     * @throws Exception that can occur during the test.
-     */
-    @Test
-    public final void mergeMesswerte() throws Exception {
-        transaction.begin();
-        Measm messung =
-            repository.getById(Measm.class, MID1200);
-        QueryBuilder<MeasVal> builder = repository
-            .queryBuilder(MeasVal.class)
-            .and(MeasVal_.measmId, messung.getId());
-        // Measm has two measVals before
-        Assert.assertEquals(2, repository.filter(builder.getQuery()).size());
-
-        MeasVal wert1 = new MeasVal();
-        wert1.setMeasmId(MID1200);
-        wert1.setMeasUnitId(MEHID207);
-        wert1.setMeasdId(MGID56);
-        wert1.setMeasVal(MESS15D);
-        merger.mergeMesswerte(messung, List.of(wert1));
-        List<MeasVal> dbWerte = repository.filter(builder.getQuery());
-        // Only the "merged" measVal is kept
-        Assert.assertEquals(1, dbWerte.size());
-        transaction.commit();
-
-        shouldMatchDataSet(
-            "datasets/dbUnit_import_merge_match_messwert.xml",
-            "lada.meas_val",
-            new String[]{"id", "last_mod", "tree_mod"});
     }
 
     /**
---
+++ src/test/java/de/intevation/lada/importer/IdentifierTest.java
@@ -0,0 +1,224 @@
+/* Copyright (C) 2025 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY! Check out
+ * the documentation coming with IMIS-Labordaten-Application for details.
+ */
+package de.intevation.lada.importer;
+
+import jakarta.annotation.Resource;
+import jakarta.inject.Inject;
+import jakarta.transaction.UserTransaction;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.transaction.api.annotation.Transactional;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import de.intevation.lada.BaseTest;
+import de.intevation.lada.model.lada.Measm;
+import de.intevation.lada.model.lada.Sample;
+
+
+/**
+ * Unit tests for Identifier implementations.
+ */
+@RunWith(Arquillian.class)
+public class IdentifierTest extends BaseTest {
+
+    private static final int PID1000 = 1000;
+
+    @Resource
+    private UserTransaction transaction;
+
+    private final String mstId = "06010";
+
+    @Inject
+    private Identifier<Sample> probeIdentifier;
+
+    @Inject
+    private Identifier<Measm> messungIdentifier;
+
+    public IdentifierTest() {
+        testDatasetName = "datasets/dbUnit_identifier.xml";
+    }
+
+    /**
+     * Identify probe objects.
+     *
+     * @throws Exception that can occur during the test.
+     */
+    @Test
+    @Transactional
+    public final void identifyProbeByHPNrMST() throws Exception {
+        Sample probe = new Sample();
+        probe.setMainSampleId("120510002");
+        probe.setMeasFacilId(mstId);
+        probe.setIsTest(false);
+
+        Assert.assertNotNull(probeIdentifier.getExisting(probe));
+    }
+
+    /**
+     * Identify probject by HP-Nr and MST.
+     * @throws Exception that can occur during the test.
+     */
+    @Test
+    @Transactional
+    public final void identifyProbeByHPNrMSTNew() throws Exception {
+        Sample probe = new Sample();
+        probe.setMainSampleId("120510003");
+        probe.setMeasFacilId(mstId);
+
+        Assert.assertNull(probeIdentifier.getExisting(probe));
+    }
+
+    /**
+     * Identify probe object by external probe id.
+     *
+     * @throws Exception that can occur during the test.
+     */
+    @Test
+    @Transactional
+    public final void identifyProbeByExterneProbeId() throws Exception {
+        Sample probe = new Sample();
+        probe.setExtId("T001");
+
+        Assert.assertNotNull(probeIdentifier.getExisting(probe));
+    }
+
+    /**
+     * Identify probe object by external id as new.
+     * @throws Exception that can occur during test.
+     */
+    @Test
+    @Transactional
+    public final void identifyProbeByExterneProbeIdNew() throws Exception {
+        Sample probe = new Sample();
+        probe.setExtId("T002");
+
+        Assert.assertNull(probeIdentifier.getExisting(probe));
+    }
+
+    /**
+     * Identify probe object by external id as reject.
+     * @throws Exception that can occur during the test.
+     */
+    @Test
+    @Transactional
+    public final void identifyProbeByExterneProbeIdReject() throws Exception {
+        Sample probe = new Sample();
+        probe.setExtId("T001");
+        probe.setMainSampleId("120510003");
+        probe.setMeasFacilId(mstId);
+
+        Assert.assertThrows(
+            Identifier.IdentificationException.class,
+            () -> probeIdentifier.getExisting(probe));
+    }
+
+    /**
+     * Identify probe object by external id as update.
+     * @throws Exception that ca occur during the test.
+     */
+    @Test
+    @Transactional
+    public final void identifyProbeByExterneProbeIdUpdate() throws Exception {
+        Sample probe = new Sample();
+        probe.setExtId("T001");
+        probe.setMainSampleId("");
+        probe.setMeasFacilId(mstId);
+
+        Assert.assertNotNull(probeIdentifier.getExisting(probe));
+    }
+
+    /**
+     * Identify messung object by np nr.
+     * @throws Exception that can occur during the test.
+     */
+    @Test
+    @Transactional
+    public final void identifyMessungByNpNr() throws Exception {
+        Measm messung = new Measm();
+        messung.setSampleId(PID1000);
+        messung.setMinSampleId("06A0");
+
+        Assert.assertNotNull(messungIdentifier.getExisting(messung));
+    }
+
+    /**
+     * Identify messung object by np nr. as new.
+     * @throws Exception that can occur during the test.
+     */
+    @Test
+    @Transactional
+    public final void identifyMessungByNpNrNew() throws Exception {
+        Measm messung = new Measm();
+        messung.setSampleId(PID1000);
+        messung.setMinSampleId("06A1");
+
+        Assert.assertNull(messungIdentifier.getExisting(messung));
+    }
+
+    /**
+     * Identify messung object by external id.
+     * @throws Exception that can occur during the test.
+     */
+    @Test
+    @Transactional
+    public final void identifyMessungByExterneMessungsId() throws Exception {
+        Measm messung = new Measm();
+        messung.setSampleId(PID1000);
+        messung.setExtId(1);
+
+        Assert.assertNotNull(messungIdentifier.getExisting(messung));
+    }
+
+    /**
+     * Identify messung object by external id as new.
+     * @throws Exception that can occur during the test.
+     */
+    @Test
+    @Transactional
+    public final void identifyMessungByExterneMessungsIdNew() throws Exception {
+        Measm messung = new Measm();
+        messung.setSampleId(PID1000);
+        messung.setExtId(2);
+
+        Assert.assertNull(messungIdentifier.getExisting(messung));
+    }
+
+    /**
+     * Unidentifiable Measm object.
+     * @throws Exception that can occur during the test.
+     */
+    @Test
+    @Transactional
+    public final void identifyMessungReject()
+    throws Exception {
+        Measm messung = new Measm();
+        messung.setSampleId(PID1000);
+
+        Assert.assertThrows(
+            Identifier.IdentificationException.class,
+            () -> messungIdentifier.getExisting(messung));
+    }
+
+    /**
+     * Identify messung object by external id as update.
+     * @throws Exception that can occur during the test.
+     */
+    @Test
+    @Transactional
+    public final void identifyMessungByExterneMessungsIdUpdate()
+    throws Exception {
+        Measm messung = new Measm();
+        messung.setSampleId(PID1000);
+        messung.setExtId(1);
+        messung.setMinSampleId("");
+
+        Assert.assertNotNull(messungIdentifier.getExisting(messung));
+    }
+}
