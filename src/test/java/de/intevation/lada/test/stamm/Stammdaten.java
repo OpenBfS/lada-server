62f3ff80ca80e6cccc33db1e58df953c3cece6a1
Tom Gottfried <tom@intevation.de>
2025-08-29 12:16:17 +0200

    Simplify test
    Just verifying the keys in response objects against a set of hard
    coded strings does not seem to test any functionality of the
    application itself. Serialization is provided by the container
    and declaring fields twice has no benefit.

--- src/test/java/de/intevation/lada/test/stamm/Stammdaten.java
+++ src/test/java/de/intevation/lada/test/stamm/Stammdaten.java
@@ -7,17 +7,13 @@
  */
 package de.intevation.lada.test.stamm;
 
-import java.util.HashMap;
-import java.util.Map;
+import static org.junit.Assert.assertTrue;
 
 import jakarta.json.JsonObject;
 import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.Response;
 
 import org.junit.Assert;
-import org.hamcrest.Matcher;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
 
 import de.intevation.lada.BaseTest;
 import de.intevation.lada.ClientBaseTest;
@@ -29,216 +25,9 @@ import de.intevation.lada.test.ServiceTest;
  */
 public class Stammdaten extends ServiceTest {
 
-    private static Map<String, Matcher<Iterable<?>>> matchers;
-
     @Override
     public void init(WebTarget t) {
         super.init(t);
-
-        matchers = new HashMap<>();
-        matchers.put("regulation",
-            Matchers.containsInAnyOrder(
-                "id",
-                "lastMod",
-                "descr",
-                "name"
-            )
-        );
-        matchers.put("measunit",
-            Matchers.containsInAnyOrder(
-                "id",
-                "lastMod",
-                "name",
-                "unitSymbol",
-                "eudfUnitId",
-                "eudfConversFactor",
-                "primary"
-            )
-        );
-        matchers.put("measd",
-            Matchers.containsInAnyOrder(
-                "id",
-                "lastMod",
-                "descr",
-                "defColor",
-                "eudfNuclId",
-                "idfExtId",
-                "isRefNucl",
-                "bvlFormatId",
-                "idOld"
-            )
-        );
-        matchers.put("mmt",
-            Matchers.containsInAnyOrder(
-                "id",
-                "lastMod",
-                "descr",
-                "name"
-            )
-        );
-        matchers.put("measfacil",
-            Matchers.containsInAnyOrder(
-                "id",
-                "lastMod",
-                "trunkCode",
-                "address",
-                "name",
-                "measFacilType",
-                "networkId"
-            )
-        );
-        matchers.put("network",
-            Matchers.containsInAnyOrder(
-                "id",
-                "lastMod",
-                "isActive",
-                "idfNetworkId",
-                "isFmn",
-                "mailList",
-                "name"
-            )
-        );
-        matchers.put("pflichtmessgroesse",
-            Matchers.containsInAnyOrder(
-                "id",
-                "letzteAenderung",
-                "messgroesseId",
-                "datenbasisId",
-                "mmtId",
-                "umwId"
-            )
-        );
-        matchers.put("samplemeth",
-            Matchers.containsInAnyOrder(
-                "id",
-                "lastMod",
-                "name",
-                "extId",
-                "eudfSampleMethId"
-            )
-        );
-        matchers.put("samplespecif",
-            Matchers.containsInAnyOrder(
-                "id",
-                "lastMod",
-                "name",
-                "eudfKeyword",
-                "extId",
-                "measUnitId"
-            )
-        );
-        matchers.put("location",
-            Matchers.containsInAnyOrder(
-                "id",
-                "beschreibung",
-                "bezeichnung",
-                "hoeheLand",
-                "koordXExtern",
-                "koordYExtern",
-                "latitude",
-                "longitude",
-                "letzteAenderung",
-                "unscharf",
-                "netzbetreiberId",
-                "staatId",
-                "verwaltungseinheitId",
-                "otyp",
-                "koordinatenartId",
-                "geom"
-            )
-        );
-        matchers.put("spatrefsys",
-            Matchers.containsInAnyOrder(
-                "id",
-                "lastMod",
-                "idfGeoKey",
-                "name"
-            )
-        );
-        matchers.put("typeregulation",
-            Matchers.containsInAnyOrder(
-            "id",
-            "name",
-            "lastMod"
-            )
-        );
-        matchers.put("poi",
-            Matchers.containsInAnyOrder(
-            "id",
-            "name",
-            "lastMod"
-            )
-        );
-        matchers.put("state",
-            Matchers.containsInAnyOrder(
-                "id",
-                "lastMod",
-                "isEuCountry",
-                "ctryOrigId",
-                "coordXExt",
-                "coordYExt",
-                "ctry",
-                "iso3166",
-                "intVehRegCode",
-                "spatRefSysId"
-            )
-        );
-        matchers.put("targactmmtgr",
-            Matchers.containsInAnyOrder(
-                "id",
-                "name",
-                "targEnvGrDispl"
-            )
-        );
-        matchers.put("targenvgr",
-            Matchers.containsInAnyOrder(
-                "id",
-                "name",
-                "descr"
-            )
-        );
-        matchers.put("envmedium",
-            Matchers.containsInAnyOrder(
-                "id",
-                "lastMod",
-                "descr",
-                "name",
-                "unit1",
-                "unit2"
-            )
-        );
-        matchers.put("envspecifmp",
-            Matchers.containsInAnyOrder(
-                "id",
-                "sampleSpecifId",
-                "envMediumId",
-                "lastMod"
-            )
-        );
-        matchers.put("adminunit",
-            Matchers.containsInAnyOrder(
-                "id",
-                "name",
-                "stateId",
-                "isState",
-                "isMunic",
-                "isRuralDist",
-                "isGovDist",
-                "ruralDistId",
-                "latitude",
-                "longitude",
-                "zip",
-                "govDistId"
-            )
-        );
-        matchers.put("reiaggr",
-            Matchers.containsInAnyOrder(
-                "id",
-                "name",
-                "descr",
-                "lastMod"
-            )
-        );
     }
 
     /**
@@ -267,8 +56,7 @@ public class Stammdaten extends ServiceTest {
             .header("X-SHIB-user", BaseTest.testUser)
             .header("X-SHIB-roles", BaseTest.testRoles)
             .get();
-        JsonObject content = ClientBaseTest.parseResponse(response).asJsonObject();
-        /* Verify the response*/
-        MatcherAssert.assertThat(content.keySet(), matchers.get(type));
+        assertTrue("Response should contain an object",
+            ClientBaseTest.parseResponse(response) instanceof JsonObject);
     }
 }
