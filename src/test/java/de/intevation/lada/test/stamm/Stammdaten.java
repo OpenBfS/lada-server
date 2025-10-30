e71d872b584c2846d56651e15a86c8e2bfa102bf
Tom Gottfried <tom.gottfried@intevation.de>
2025-10-30 22:15:14 +0100

    Add service for human readable descriptions of statusMpIds
    While at it, replace some hard coded strings in test code.

--- src/test/java/de/intevation/lada/test/stamm/Stammdaten.java
+++ src/test/java/de/intevation/lada/test/stamm/Stammdaten.java
@@ -7,6 +7,7 @@
  */
 package de.intevation.lada.test.stamm;
 
+import static de.intevation.lada.rest.LadaService.PATH_REST;
 import static org.junit.Assert.assertTrue;
 
 import jakarta.json.JsonObject;
@@ -35,11 +36,9 @@ public class Stammdaten extends ServiceTest {
      *
      * @param type the entity type.
      */
-    public final void getAll(
-        String type
-    ) {
+    public final void getAll(String type) {
         Assert.assertNotNull(type);
-        get("rest/" + type);
+        get(PATH_REST + type);
     }
 
     /**
@@ -52,7 +51,7 @@ public class Stammdaten extends ServiceTest {
         Object id
     ) {
         /* Request an object by id*/
-        Response response = target.path("rest/" + type + "/" + id).request()
+        Response response = target.path(PATH_REST + type + "/" + id).request()
             .header("X-SHIB-user", BaseTest.testUser)
             .header("X-SHIB-roles", BaseTest.testRoles)
             .get();
--- src/test/java/de/intevation/lada/test/stamm/StatusMpTest.java
+++ src/test/java/de/intevation/lada/test/stamm/StatusMpTest.java
@@ -11,14 +11,26 @@ import jakarta.json.Json;
 import jakarta.json.JsonArray;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.GenericType;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriBuilder;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
 
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
 
 import de.intevation.lada.BaseTest;
 import de.intevation.lada.ClientBaseTest;
+import de.intevation.lada.model.master.StatusLev_;
+import de.intevation.lada.model.master.StatusMpView;
+import de.intevation.lada.model.master.StatusMp_;
+import de.intevation.lada.model.master.StatusVal_;
+import de.intevation.lada.rest.StatusMpService;
+import de.intevation.lada.rest.StatusMpViewService;
 import de.intevation.lada.test.ServiceTest;
 
 /**
@@ -26,7 +38,10 @@ import de.intevation.lada.test.ServiceTest;
  */
 public class StatusMpTest extends ServiceTest {
 
-    private static final String URL = "rest/statusmp/";
+    private static final String STATUS_MP_URL = UriBuilder
+        .fromResource(StatusMpService.class).build() + "/";
+    private static final String STATUS_MP_VIEW_URL = UriBuilder
+        .fromResource(StatusMpViewService.class).build() + "/";
 
     @Override
     public void init(WebTarget t) {
@@ -37,19 +52,31 @@ public class StatusMpTest extends ServiceTest {
      * Execute the tests.
      */
     public void execute() {
-        get(URL);
+        get(STATUS_MP_URL);
 
-        getById(URL + "1", Json.createObjectBuilder()
-            .add("id", 1)
-            .add("statusLev", Json.createObjectBuilder()
-                .add("id", 1).add("lev", "MST"))
-            .add("statusVal", Json.createObjectBuilder()
-                .add("id", 0).add("val", "nicht vergeben"))
+        // Test endpoint to get value by statusMpId
+        final int statusMpId = 1;
+        final String lev = "MST", val = "nicht vergeben";
+        getById(STATUS_MP_URL + statusMpId, Json.createObjectBuilder()
+            .add(StatusMp_.ID, statusMpId)
+            .add(StatusMp_.STATUS_LEV, Json.createObjectBuilder()
+                .add(StatusLev_.ID, 1).add(StatusLev_.LEV, lev))
+            .add(StatusMp_.STATUS_VAL, Json.createObjectBuilder()
+                .add(StatusVal_.ID, 0).add(StatusVal_.VAL, val))
             .build()
         );
 
+        // Test service for human readable descriptions
+        String statusComb = get(STATUS_MP_VIEW_URL,
+            new GenericType<List<StatusMpView>>() {})
+            .stream()
+            .filter(e -> e.getStatusMpId().equals(1))
+            .map(StatusMpView::getStatusComb).findFirst().get();
+        assertEquals(lev + " - " + val, statusComb);
+
+        // Test endpoint to get values by measmIds
         final int measmId = 1801;
-        Response response = target.path(URL + "getbyids")
+        Response response = target.path(STATUS_MP_URL + "getbyids")
             .request()
             .header("X-SHIB-user", BaseTest.testUser)
             .header("X-SHIB-roles", BaseTest.testRoles)
@@ -63,18 +90,19 @@ public class StatusMpTest extends ServiceTest {
             reachable,
             CoreMatchers.hasItems(
                 Json.createObjectBuilder()
-                .add("id", 2)
-                .add("statusLev", Json.createObjectBuilder()
-                    .add("id", 1).add("lev", "MST"))
-                .add("statusVal", Json.createObjectBuilder()
-                    .add("id", 1).add("val", "plausibel"))
+                .add(StatusMp_.ID, 2)
+                .add(StatusMp_.STATUS_LEV, Json.createObjectBuilder()
+                    .add(StatusLev_.ID, 1).add(StatusLev_.LEV, lev))
+                .add(StatusMp_.STATUS_VAL, Json.createObjectBuilder()
+                    .add(StatusVal_.ID, 1).add(StatusVal_.VAL, "plausibel"))
                 .build(),
                 Json.createObjectBuilder()
-                .add("id", 3)
-                .add("statusLev", Json.createObjectBuilder()
-                    .add("id", 1).add("lev", "MST"))
-                .add("statusVal", Json.createObjectBuilder()
-                    .add("id", 2).add("val", "nicht repräsentativ"))
+                .add(StatusMp_.ID, 3)
+                .add(StatusMp_.STATUS_LEV, Json.createObjectBuilder()
+                    .add(StatusLev_.ID, 1).add(StatusLev_.LEV, lev))
+                .add(StatusMp_.STATUS_VAL, Json.createObjectBuilder()
+                    .add(StatusVal_.ID, 2)
+                    .add(StatusVal_.VAL, "nicht repräsentativ"))
                 .build()));
     }
 }
