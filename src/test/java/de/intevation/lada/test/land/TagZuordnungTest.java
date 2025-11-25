44bae9a8e7ed749c9e8400bc01dd57e7b6a74822
Tom Gottfried <tom@intevation.de>
2025-11-25 15:25:35 +0100

    Improve testing tag links
    Reduce hard coded strings and ensure associations are actually deleted.

--- src/test/java/de/intevation/lada/test/land/TagZuordnungTest.java
+++ src/test/java/de/intevation/lada/test/land/TagZuordnungTest.java
@@ -7,30 +7,50 @@
  */
 package de.intevation.lada.test.land;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
+import jakarta.json.Json;
 import jakarta.json.JsonArray;
 import jakarta.json.JsonObject;
-import jakarta.json.JsonValue;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.GenericType;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriBuilder;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.junit.Assert;
 
 import de.intevation.lada.BaseTest;
 import de.intevation.lada.ClientBaseTest;
+import de.intevation.lada.model.lada.TagLink_;
+import de.intevation.lada.model.master.Tag;
+import de.intevation.lada.rest.TagLinkMeasmService;
+import de.intevation.lada.rest.TagLinkSampleService;
+import de.intevation.lada.rest.TagService;
 import de.intevation.lada.test.ServiceTest;
 import de.intevation.lada.test.stamm.TagTest;
+import de.intevation.lada.util.data.StatusCodes;
 
 /**
  * Test tagzuordnung entities.
  */
 public class TagZuordnungTest extends ServiceTest {
-    private final String tagUrlSample = "rest/tag/taglinksample/";
-    private final String tagUrlMeasm = "rest/tag/taglinkmeasm/";
+
+    private final UriBuilder tagUrl = UriBuilder
+        .fromResource(TagService.class);
+    private final UriBuilder tagUrlSample = UriBuilder
+        .fromResource(TagLinkSampleService.class);
+    private final UriBuilder tagUrlMeasm = UriBuilder
+        .fromResource(TagLinkMeasmService.class);
+
+    private final String sampleIdParam = "sampleId";
+    private final String measmIdParam = "measmId";
 
     private JsonArray createSample;
     private JsonArray createMeasm;
@@ -66,78 +86,125 @@ public class TagZuordnungTest extends ServiceTest {
         // Should accept existing tag links
         bulkOperation(tagUrlSample, createSample);
 
-        JsonArray tagResponse = get("rest/tag/").asJsonArray();
+        List<Tag> tagResponse = getTags(null);
         List<Integer> tagIds = createSample.stream()
-            .map(zuord -> zuord.asJsonObject().getInt("tagId"))
+            .map(zuord -> zuord.asJsonObject().getInt(TagLink_.TAG_ID))
             .collect(Collectors.toList());
-        List<JsonValue> tags = tagResponse.stream()
-            .filter(tag -> tagIds.contains(
-                    tag.asJsonObject().getInt("id")))
+        List<Tag> tags = tagResponse.stream()
+            .filter(tag -> tagIds.contains(tag.getId()))
             .collect(Collectors.toList());
         Assert.assertEquals(2, tags.size());
 
         // Test validity of newly assigned tags
-        tags.forEach(tagVal -> {
-            JsonObject tag = (JsonObject) tagVal;
-            String gueltigBisLong = tag.getString("valUntil");
-            long diffInDays = getDaysFromNow(gueltigBisLong);
+        tags.forEach(tag -> {
+            long diffInDays = getDaysFromNow(tag.getValUntil());
             Assert.assertEquals(TagTest.MF_TAG_EXPIRATION_DAYS, diffInDays);
         });
 
         // test filtering tags by assignment
-        tagResponse = get("rest/tag?sampleId=9999").asJsonArray();
-        Assert.assertTrue(
+        final int nonExistent = 9999;
+        tagResponse = getTags(sampleIdParam, nonExistent);
+        assertTrue(
             "Returned data despite filtering for non-existent ID",
             tagResponse.isEmpty());
 
-        tagResponse = get("rest/tag?sampleId=1901").asJsonArray();
+        final int sampleId = 1901;
+        tagResponse = getTags(sampleIdParam, sampleId);
         Assert.assertEquals(
             "Number of tags for given Sample ID:",
             3, tagResponse.size());
 
-        tagResponse = get("rest/tag?measmId=1801").asJsonArray();
+        final int measmId1 = 1801;
+        tagResponse = getTags(measmIdParam, measmId1);
         Assert.assertEquals(
             "Number of tags for given Messung ID:",
             1, tagResponse.size());
 
-        tagResponse = get("rest/tag?measmId=1801&measmId=1802").asJsonArray();
-        Assert.assertTrue(
-            "Expected empty result filtering by tagged and un-tagged object",
-            tagResponse.isEmpty());
+        final int measmId2 = 1802;
+        tagResponse = getTags(measmIdParam, measmId1, measmId2);
+        assertTrue("Expected empty result", tagResponse.isEmpty());
 
         bulkOperation(tagUrlMeasm, create2);
-        tagResponse = get("rest/tag?measmId=1801&measmId=1802").asJsonArray();
+        tagResponse = getTags(measmIdParam, measmId1, measmId2);
         Assert.assertEquals(
             "Number of tags for given Messung IDs:",
             1, tagResponse.size());
 
-        // Test unassigning tags
-        bulkOperation(tagUrlSample + "delete", createSample);
-        // Should accept existing tag links
-        bulkOperation(tagUrlSample + "delete", createSample);
+        final int tagId = 1002;
+        testUnassigningTags(TagLinkSampleService.class, sampleId, tagId);
+        // TODO:
+        //testUnassigningTags(TagLinkMeasmService.class, measmId1, tagId);
+    }
+
+    private void testUnassigningTags(
+        Class<?> service, int taggableId, int tagId
+    ) {
+        String param = TagLinkSampleService.class.equals(service)
+            ? sampleIdParam
+            : measmIdParam;
+        // Ensure to be deleted association exists
+        MatcherAssert.assertThat(
+            String.format(
+                "To be deleted association with %s does not exist", param),
+            getTags(param, taggableId).stream().map(Tag::getId).toList(),
+            CoreMatchers.hasItem(tagId)
+        );
+
+        // Delete association
+        UriBuilder deleteUri = UriBuilder.fromResource(service)
+            .path(service, "deleteTagReference");
+        JsonArray payload = Json.createArrayBuilder()
+            .add(Json.createObjectBuilder()
+                .add(param, taggableId)
+                .add(TagLink_.TAG_ID, tagId))
+            .build();
+        bulkOperation(deleteUri, payload);
+
+        // Ensure deleted association no longer exists
+        MatcherAssert.assertThat(
+            String.format(
+                "To be deleted association with %s still exists", param),
+            getTags(param, taggableId).stream().map(Tag::getId).toList(),
+            CoreMatchers.not(CoreMatchers.hasItem(tagId))
+        );
+
+        // Should accept non-existent (already deleted) tag links
+        bulkOperation(deleteUri, payload);
     }
 
     private void bulkOperation(
-        String parameter, JsonArray payload
+        UriBuilder path, JsonArray payload
     ) {
-        Response response = target.path(parameter)
+        Response response = target.path(path.build().toString())
             .request()
             .header("X-SHIB-user", BaseTest.testUser)
             .header("X-SHIB-roles", BaseTest.testRoles)
             .accept(MediaType.APPLICATION_JSON)
             .post(Entity.entity(
                     payload.toString(), MediaType.APPLICATION_JSON));
-        JsonArray content = ClientBaseTest.parseResponse(response).asJsonArray();
+        JsonArray content = ClientBaseTest.parseResponse(response)
+            .asJsonArray();
+
         //Check each result
         final String successKey = "success", messageKey = "message";
         content.forEach(object -> {
             JsonObject responseObj = (JsonObject) object;
             BaseTest.assertContains(responseObj, successKey);
-            Assert.assertTrue(
+            assertTrue(
                 "Unsuccessful response list element:\n" + responseObj,
                 responseObj.getBoolean(successKey));
             BaseTest.assertContains(responseObj, messageKey);
-            Assert.assertEquals("200", responseObj.getString(messageKey));
+            Assert.assertEquals(
+                String.valueOf(StatusCodes.OK),
+                responseObj.getString(messageKey));
         });
+    }
+
+    private List<Tag> getTags(String param, Object... values) {
+        UriBuilder url = tagUrl.clone();
+        if (param != null) {
+            url.queryParam(param, values);
+        }
+        return get(url.build().toString(), new GenericType<List<Tag>>() { });
     }
 }
