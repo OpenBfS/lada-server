38930004a07881bfc8ad63b6f9e611b13f03d6a2
Tom Gottfried <tom@intevation.de>
2025-12-08 14:19:36 +0100

    Fix handling deleted measurements
    Repository::getById throws NotFoundException if the measurement
    no longer exists.

--- src/main/java/de/intevation/lada/rest/AuditTrailService.java
+++ src/main/java/de/intevation/lada/rest/AuditTrailService.java
@@ -266,7 +266,7 @@ public class AuditTrailService extends LadaService {
             node.setIdentifier(value);
             break;
         case "measm":
-            Measm m = repository.getById(
+            Measm m = repository.entityManager().find(
                 Measm.class, audit.getObjectId());
             node.setIdentifier(getIdentifier(m));
             break;
@@ -275,7 +275,7 @@ public class AuditTrailService extends LadaService {
         }
 
         if (audit.getMeasmId() != null) {
-            Measm m = repository.getById(
+            Measm m = repository.entityManager().find(
                 Measm.class, audit.getMeasmId());
             AuditEntryIdentifier identifier = new AuditEntryIdentifier();
             identifier.setMeasm(getIdentifier(m));
