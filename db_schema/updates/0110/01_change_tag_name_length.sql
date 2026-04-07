a36e852aa3645ab707640f17c30d8cef9cf3c605
Tom Gottfried <tom@intevation.de>
2026-04-07 12:14:18 +0200

    Fixup rev. a316f7b2
    Cannot change type of column a view depends on.

--- db_schema/updates/0110/01_change_tag_name_length.sql
+++ db_schema/updates/0110/01_change_tag_name_length.sql
@@ -1,1 +1,14 @@
+DROP VIEW stamm.tag;
+
 ALTER TABLE master.tag ALTER COLUMN name TYPE VARCHAR(32);
+
+CREATE VIEW stamm.tag AS SELECT
+	id,
+	name AS tag,
+	meas_facil_id AS mst_id,
+	is_auto_tag AS auto_tag,
+	network_id AS netzbetreiber_id,
+	lada_user_id AS user_id,
+	val_until AS gueltig_bis,
+	created_at
+FROM master.tag;
