33c1e117a361f2594112bd1eac3e1bc2b87d7942
Tom Gottfried <tom@intevation.de>
2026-04-24 19:27:50 +0200

    Replace CLI script with environment variables
    Frees the shell script from CLI overhead. See also
    https://docs.wildfly.org/39/Admin_Guide.html#overridden-attribute-value

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,16 +10,11 @@ LADA_JBOSS_HOME=${LADA_JBOSS_HOME:-/opt/jboss/wildfly}
 LADA_JBOSS_ADMIN=${LADA_JBOSS_ADMIN:-ADMIN}
 LADA_JBOSS_PW=${LADA_JBOSS_PW:-secret}
 
-$JBOSS_HOME/bin/jboss-cli.sh <<EOF
-embed-server
-batch
-/subsystem=datasources/data-source=lada:write-attribute(name=connection-url,value=jdbc:postgresql://${LADA_DB_SRV}:${LADA_DB_PORT}/${LADA_DB_NAME})
-/subsystem=datasources/data-source=lada:write-attribute(name=user-name,value=${LADA_DB_USER})
-/subsystem=datasources/data-source=lada:write-attribute(name=password,value=${LADA_DB_PW})
-run-batch
-stop-embedded-server
-EOF
+export WILDFLY_OVERRIDING_ENV_VARS=1
 
+export SUBSYSTEM_DATASOURCES_DATA_SOURCE_LADA__CONNECTION_URL=jdbc:postgresql://${LADA_DB_SRV}:${LADA_DB_PORT}/${LADA_DB_NAME}
+export SUBSYSTEM_DATASOURCES_DATA_SOURCE_LADA__USER_NAME=${LADA_DB_USER}
+export SUBSYSTEM_DATASOURCES_DATA_SOURCE_LADA__PASSWORD=${LADA_DB_PW}
 
 export JAVA_OPTS='${LADA_JAVA_OPTS}'
 /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
